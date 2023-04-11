eaf6ddb5cb3443de07c6ddc6e0a0e529718299e2
Paul Schwind <git@pschwind.de>
2023-04-11 21:07:17 +0200

    add import

--- decorators/submission.py
+++ decorators/submission.py
@@ -1,5 +1,6 @@
 from athena.submission import Submission, on_submission
 from athena.storage import store_submission
+from .clustering import determine_cluster
 
 @on_submission(new_only=True) # Alternatives: on_submission(), on_submission(update_only=False)
 def process_submission(submission: Submission):
