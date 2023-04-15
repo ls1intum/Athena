a18ab5ee9730ae008151cee054433516e2bb907e
Paul Schwind <git@pschwind.de>
2023-04-16 00:17:09 +0200

    dockerization

--- athena/__init__.py
+++ athena/__init__.py
@@ -1,11 +1,13 @@
+import os
+
 from fastapi import FastAPI
 import uvicorn
 
 from .models import Exercise, Submission, Feedback
-
 from .feedback_consumer import feedback_consumer
 from .submissions_consumer import submissions_consumer
 from .feedback_provider import feedback_provider
+from .logger import logger
 
 _app = FastAPI()
 
@@ -14,7 +16,13 @@ def read_root():
     return {"athene": "module"}
 
 def start():
-    uvicorn.run(_app, host="127.0.0.1", port=8000)
+    logger.info("Starting athena module")
+    if "PRODUCTION" in os.environ:
+        logger.info("Running in PRODUCTION mode")
+        uvicorn.run(_app, host="127.0.0.1", port=8000, proxy_headers=True)
+    else:
+        logger.warning("Running in DEVELOPMENT mode")
+        uvicorn.run(_app, host="127.0.0.1", port=8000, reload=True)
 
 __all__ = [
     "Exercise",
