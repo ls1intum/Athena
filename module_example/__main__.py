5f5d4cdf0e6cfcabf976c05fb8c054315000881d
Paul Schwind <git@pschwind.de>
2023-04-18 19:21:33 +0200

    move example module code into one file

--- module_example/__main__.py
+++ module_example/__main__.py
@@ -1,6 +1,42 @@
-from athena import app
-from module_example import consume_feedback, consume_submissions, provide_feedback
+from typing import List
 
-__all__ = ["consume_feedback", "consume_submissions", "provide_feedback"]
+from athena import *
+from athena.storage import *
+
+
+def is_in_same_cluster(submission1: Submission, submission2: Submission) -> bool:
+    return submission1.meta.get("cluster") == submission2.meta.get("cluster")
+
+@feedback_consumer
+def process_incoming_feedback(exercise: Exercise, submission: Submission, feedback: Feedback):
+    print(f"process_feedback: Received feedback for submission {submission.id} of exercise {exercise.id}.")
+    # find similar submissions
+    for submission in get_stored_submissions(feedback.exercise_id):
+        if submission.student_id == feedback.student_id:
+            continue
+        if is_in_same_cluster(submission, feedback.submission):
+            # reuse feedback
+            adjusted_feedback = feedback.copy(submission=submission)
+            store_feedback_suggestion(adjusted_feedback)
+
+
+def determine_cluster(submission: Submission) -> str:
+    return "cluster1"
+
+
+@submissions_consumer
+def receive_submissions(exercise: Exercise, submissions: List[Submission]):
+    print(f"Received {len(submissions)} submissions for exercise {exercise.id}")
+    for submission in submissions:
+        submission.meta["cluster"] = determine_cluster(submission)
+        store_submission(submission)
+
+
+@feedback_provider
+def suggest_feedback(exercise: Exercise, submission: Submission) -> Feedback:
+    print(f"suggest_feedback: Received submission {submission.id} of exercise {exercise.id}.")
+    return 
+
+
 if __name__ == "__main__":
     app.start()
