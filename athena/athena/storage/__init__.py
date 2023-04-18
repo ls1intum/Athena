5c7a3afca809bd70565ba0dcb96ab0b8090b0512
Paul Schwind <git@pschwind.de>
2023-04-19 01:58:06 +0200

    pycharm config

--- athena/athena/storage/__init__.py
+++ athena/athena/storage/__init__.py
@@ -1,13 +1,16 @@
 from typing import List, Union
 from ..models import Submission, Feedback
 
+
 def get_stored_submissions(exercise_id: int) -> List[Submission]:
     """Returns a list of submissions for the given exercise."""
     return []
 
+
 def store_submission(submission: Submission):
     """Stores the given submission."""
     pass
+
 
 def get_stored_feedback(exercise_id: int, submission_id: Union[int, None]) -> List[Feedback]:
     """
@@ -16,13 +19,16 @@ def get_stored_feedback(exercise_id: int, submission_id: Union[int, None]) -> Li
     """
     return []
 
+
 def store_feedback(feedback: Feedback):
     """Stores the given feedback."""
     pass
 
+
 def store_feedback_suggestion(feedback: Feedback):
     """Stores the given feedback as a suggestion."""
     pass
+
 
 def get_stored_feedback_suggestions(exercise_id: str, submission_id: int) -> List[Feedback]:
     """Returns a list of feedback suggestions for the given exercise in the given submission."""
