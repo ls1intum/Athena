bf7608998d95e6654080b974b293f594be108e65
Paul Schwind <git@pschwind.de>
2023-04-19 02:05:41 +0200

    nicer way to add app.start function

--- athena/athena/__init__.py
+++ athena/athena/__init__.py
@@ -1,12 +1,8 @@
 import os
 from pathlib import Path
-import inspect
 import runpy
 
-import uvicorn
-
 from .app import app
-from .logger import logger
 from .models import Exercise, Submission, Feedback
 from .feedback_consumer import feedback_consumer
 from .submissions_consumer import submissions_consumer
@@ -16,25 +12,6 @@ from .feedback_provider import feedback_provider
 @app.get("/")
 def read_root():
     return {"athena": "module"}
-
-
-def start(port: int = 5001):
-    """Start athena. Ensure to have `app` in your module main scope so that it can be imported."""
-    logger.info("Starting athena module")
-
-    importing_file = inspect.stack()[1].filename # one stack frame up to find it
-    module_name = Path(importing_file).parent.name # get the parent directory name = module name
-
-    if "PRODUCTION" in os.environ:
-        logger.info("Running in PRODUCTION mode")
-        uvicorn.run(f"{module_name}.__main__:app", host="0.0.0.0", port=port, proxy_headers=True)
-    else:
-        logger.warning("Running in DEVELOPMENT mode")
-        uvicorn.run(f"{module_name}.__main__:app", host="0.0.0.0", port=port, reload=True)
-
-
-# Expose the start function this way to ensure that modules have to import `app`, which uvicorn needs to discover in the module:
-app.start = start
 
 
 def run_module():
--- athena/athena/app.py
+++ athena/athena/app.py
@@ -1,3 +1,33 @@
-from fastapi import FastAPI
+from .logger import logger
+import inspect
+import os
+from pathlib import Path
 
-app = FastAPI()
+from fastapi import FastAPI
+import uvicorn
+
+
+class FastAPIWithStart(FastAPI):
+    """
+    Athena provides a FastAPI instance with an additional start method.
+    We expose the start function this way to ensure that modules have to import `app`,
+    which uvicorn needs to discover in the module.
+    """
+    def start(self, port: int = 5001) -> None:
+        """Start Athena. You have to ensure to have `app` in your module main scope so that it can be imported."""
+        logger.info("Starting athena module")
+
+        importing_file = inspect.stack()[1].filename  # one stack frame up to find it
+        module_name = Path(importing_file).parent.name  # get the parent directory name = module name
+
+        if "PRODUCTION" in os.environ:
+            logger.info("Running in PRODUCTION mode")
+            uvicorn.run(f"{module_name}.__main__:app", host="0.0.0.0", port=port, proxy_headers=True)
+        else:
+            logger.warning("Running in DEVELOPMENT mode")
+            uvicorn.run(f"{module_name}.__main__:app", host="0.0.0.0", port=port, reload=True)
+
+
+app: FastAPIWithStart = FastAPIWithStart()
+
+
