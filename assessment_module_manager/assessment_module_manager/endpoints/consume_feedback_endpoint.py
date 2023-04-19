1d45704d7a5baf5107430b5331d22f6f5044d100
Paul Schwind <git@pschwind.de>
2023-04-19 21:43:39 +0200

    use 204 to express no content

--- assessment_module_manager/assessment_module_manager/endpoints/consume_feedback_endpoint.py
+++ assessment_module_manager/assessment_module_manager/endpoints/consume_feedback_endpoint.py
@@ -1,16 +1,16 @@
-from typing import Literal
+from fastapi import status
 
 from assessment_module_manager.app import app
 from assessment_module_manager.module import resolve_module, request_to_module
 from athena import Exercise, Submission, Feedback
 
 
-@app.post('/feedback', responses={
+@app.post('/feedback', status_code=status.HTTP_204_NO_CONTENT, responses={
     503: {
         "description": "Module is not available",
     },
 })
-async def consume_feedback(exercise: Exercise, submission: Submission, feedback: Feedback) -> Literal['']:
+async def consume_feedback(exercise: Exercise, submission: Submission, feedback: Feedback):
     """
     This endpoint is called by the LMS whenever there is a new feedback to process.
     """
@@ -23,4 +23,3 @@ async def consume_feedback(exercise: Exercise, submission: Submission, feedback:
             'feedback': feedback.dict(),
         },
     )
-    return ''
--- assessment_module_manager/assessment_module_manager/endpoints/consume_submissions_endpoint.py
+++ assessment_module_manager/assessment_module_manager/endpoints/consume_submissions_endpoint.py
@@ -1,16 +1,16 @@
-from typing import Literal
+from fastapi import status
 
 from assessment_module_manager.app import app
 from assessment_module_manager.module import resolve_module, request_to_module
 from athena import Exercise, Submission
 
 
-@app.post('/submissions', responses={
+@app.post('/submissions', status_code=status.HTTP_204_NO_CONTENT, responses={
     503: {
         "description": "Module is not available",
     },
 })
-async def consume_submissions(exercise: Exercise, submissions: list[Submission]) -> Literal['']:
+async def consume_submissions(exercise: Exercise, submissions: list[Submission]):
     """
     This endpoint is called by the LMS when the exercise deadline is over and
     the submissions need to be processed.
@@ -23,4 +23,3 @@ async def consume_submissions(exercise: Exercise, submissions: list[Submission])
             'submissions': [submission.dict() for submission in submissions],
         },
     )
-    return ''
