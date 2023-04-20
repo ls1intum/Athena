6163fa22a0ae80f1d8799d2ce74ccc09f4b630d0
Paul Schwind <git@pschwind.de>
2023-04-20 05:11:12 +0200

    increase timeout

--- assessment_module_manager/assessment_module_manager/module/request_to_module.py
+++ assessment_module_manager/assessment_module_manager/module/request_to_module.py
@@ -24,7 +24,7 @@ async def request_to_module(module: Module, path: str, data: dict) -> ModuleResp
     It raises appropriate FastAPI HTTPException if the request fails.
     """
     try:
-        async with httpx.AsyncClient(base_url=module.url) as client:
+        async with httpx.AsyncClient(base_url=module.url, timeout=600) as client:
             response = await client.post(path, json=data)
     except httpx.ConnectError:
         raise HTTPException(status_code=503, detail=f"Module {module.name} is not available")
