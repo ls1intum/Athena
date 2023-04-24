33994a8af5897284dc2511d685ac3aab69373c48
Paul Schwind <git@pschwind.de>
2023-04-24 03:27:39 +0200

    improve on start_docker script

--- start_docker.py
+++ start_docker.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import os
 import subprocess
 import sys
@@ -28,26 +30,33 @@ def build_docker_compose_files(compose_files):
     subprocess.run(["docker-compose"] + compose_files + ["build"])
 
 
-def start_docker_compose_files(compose_files):
-    subprocess.run(["docker-compose"] + compose_files + ["up"])
+def start_docker_compose_files(compose_files, env_files):
+    processes = []
+    for compose_file, env_file in zip(compose_files, env_files):
+        process = subprocess.Popen(["docker-compose", "-f", compose_file, "--env-file", env_file, "up"])
+        processes.append(process)
+
+    # Wait for all processes to complete
+    for process in processes:
+        process.wait()
 
 
 def main():
     modules = get_module_folders(sys.argv[1:])
     valid_modules = filter_valid_modules(modules)
 
-    compose_files = ["-f", "docker-compose.yml"]
-    for module in valid_modules:
-        compose_files += ["-f", str(Path(module, "docker-compose.yml"))]
+    compose_files = ["docker-compose.yml"]
+    compose_files += [str(Path(module, "docker-compose.yml")) for module in valid_modules]
+    env_files = [str(Path("assessment_module_manager") / ".env")]
+    env_files += [str(Path(module, ".env")) for module in valid_modules]
 
     logger.info("Building docker-compose file for assessment module manager...")
-    build_docker_compose_files(["-f", "docker-compose.yml"])
-    for module in valid_modules:
-        logger.info(f"Building docker-compose.yml for module '{module}'...")
-        build_docker_compose_files(["-f", str(Path(module, "docker-compose.yml"))])
+    for module, compose_file in zip(valid_modules, compose_files):
+        logger.info(f"Building docker-compose.yml for '{module}'...")
+        build_docker_compose_files(["-f", compose_file])
 
     logger.info("Starting docker-compose files...")
-    start_docker_compose_files(compose_files)
+    start_docker_compose_files(compose_files, env_files)
 
 
 if __name__ == "__main__":
