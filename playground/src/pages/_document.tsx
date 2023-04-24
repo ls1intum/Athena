b870f933294bfd5d7b04a0b140741280dbe383cc
Paul Schwind <git@pschwind.de>
2023-04-24 03:43:42 +0200

    add playground title

--- playground/src/pages/_document.tsx
+++ playground/src/pages/_document.tsx
@@ -3,7 +3,9 @@ import {Head, Html, Main, NextScript} from 'next/document';
 export default function Document() {
     return (
         <Html lang="en">
-            <Head/>
+            <Head>
+                <title>Athena Playground</title>
+            </Head>
             <body>
             <Main/>
             <NextScript/>
