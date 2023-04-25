1882c19c29ac4e06c992da8b5c6d92e419e2cada
Paul Schwind <git@pschwind.de>
2023-04-25 20:11:11 +0200

    nicer function wrapper for submission selector

--- assessment_module_manager/assessment_module_manager/endpoints/select_submission.py
+++ assessment_module_manager/assessment_module_manager/endpoints/select_submission.py
@@ -10,16 +10,17 @@ from athena import Exercise
         "description": "Module is not available",
     },
 })
-async def select_submission(exercise: Exercise, submissions: List[int]) -> ModuleResponse[int]:
+async def select_submission(exercise: Exercise, submission_ids: List[int]) -> ModuleResponse[int]:
     """
     This endpoint is called by the LMS when a tutor wants to assess a submission.
     The LMS will pass the exercise and a list of submission IDs. The resulting submission ID will be given to the tutor.
+    If the module returns -1, the LMS will select a random submission.
     """
     return await request_to_module_by_exercise(
         exercise,
         '/select_submission',
         {
             'exercise': exercise.dict(),
-            'submissions': submissions,
+            'submission_ids': submission_ids,
         },
     )
--- athena/athena/submission_selector.py
+++ athena/athena/submission_selector.py
@@ -1,6 +1,7 @@
 """
 This module provides a decorator for submission selectors.
 """
+import inspect
 from functools import wraps
 from typing import Callable, List
 
@@ -9,28 +10,40 @@ from .models import Exercise, Submission
 from .storage import get_stored_submissions
 
 
+def wraps_except_annotations(func: Callable) -> Callable:
+    """
+    This is a replacement for functools.wraps that ignores annotations.
+    This is necessary when the signature of the wrapper function is different from the signature of the wrapped function
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs)
+    wrapper.__signature__ = inspect.signature(func)
+    return wrapper
+
+
 def submission_selector(func: Callable[[Exercise, List[Submission]], Submission]):
     """
     Receive an exercise and some (not necessarily all!) submissions from the Assessment Module Manager and
     return the submission that should ideally be assessed next.
-    If the selector returns None, the Assessment Module Manager will select a random submission.
+    If the selector returns None, the LMS will select a random submission in the end.
     """
     @app.post("/select_submission")
-    @wraps(func)
+    @wraps_except_annotations
     def wrapper(exercise: Exercise, submission_ids: List[int]) -> int:
         # The wrapper handles only transmitting submission IDs for efficiency, but the actual selection logic
         # only works with the full submission objects.
 
-        if not submission_ids:
-            # Nothing to select from
-            return -1
-
         # Get the full submission objects
         submissions = get_stored_submissions(exercise.id, submission_ids)
+        if not submissions:
+            # Nothing to select from
+            return -1
         # Select the submission
         submission = func(exercise, submissions)
 
         if submission is None:
             return -1
         return submission.id
+
     return wrapper
