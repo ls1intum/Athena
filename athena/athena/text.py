28d696ea79828f80816d9faa2ead2f3cf0fd055a
Paul Schwind <git@pschwind.de>
2023-05-06 01:02:21 +0200

    re-export storage helpers for proper typing

--- athena/athena/text.py
+++ athena/athena/text.py
@@ -1,9 +1,11 @@
 """Import this to use schemas and decorators specific to text exercises."""
+import functools
 import os
 
 from dotenv import load_dotenv
 
 from .schemas import TextExercise, TextFeedback, TextSubmission
+import athena.storage
 
 load_dotenv(".env")
 module_type = os.environ["MODULE_TYPE"]
@@ -16,6 +18,13 @@ Exercise = TextExercise
 Submission = TextSubmission
 Feedback = TextFeedback
 
+# re-export without the need to give the type of the requested schema
+get_stored_exercises = functools.partial(athena.storage.get_stored_exercises, Exercise)
+get_stored_submissions = functools.partial(athena.storage.get_stored_submissions, Submission)
+get_stored_feedback = functools.partial(athena.storage.get_stored_feedback, Feedback)
+get_stored_feedback_suggestions = functools.partial(athena.storage.get_stored_feedback_suggestions, Feedback)
+
 __all__ = [
-    "Exercise", "Submission", "Feedback"
+    "Exercise", "Submission", "Feedback",
+    "get_stored_exercises", "get_stored_submissions", "get_stored_feedback", "get_stored_feedback_suggestions",
 ]
