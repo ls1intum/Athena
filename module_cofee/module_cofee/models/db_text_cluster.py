1ab74d1a885fa54709399aae749c5aee64389dc9
Paul Schwind <git@pschwind.de>
2023-05-14 05:29:05 +0200

    properly store cluster-related model data => working cluster storage 🎉

--- module_cofee/module_cofee/models/db_text_cluster.py
+++ module_cofee/module_cofee/models/db_text_cluster.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer, Float, LargeBinary, ForeignKey, Boolean
+from sqlalchemy import Column, Integer, LargeBinary, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
 
 from athena.database import Base
@@ -7,7 +7,7 @@ from athena.database import Base
 class DBTextCluster(Base):
     __tablename__ = "text_clusters"
 
-    id = Column(String, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     probabilities = Column(LargeBinary)
     distance_matrix = Column(LargeBinary)
     disabled = Column(Boolean, default=False)
--- module_cofee/module_cofee/process_results.py
+++ module_cofee/module_cofee/process_results.py
@@ -1,5 +1,7 @@
+import struct
 from typing import List, Iterable
 
+from athena.database import get_db
 from athena.logger import logger
 from module_cofee.models.db_text_cluster import DBTextCluster
 from module_cofee.protobuf import cofee_pb2
@@ -10,25 +12,36 @@ def store_text_blocks(segments: List[cofee_pb2.Segment]):
     """Convert segments to text blocks and store them in the DB."""
     for segment in segments:
         # store text block in DB
-        DBTextBlock(
-            id=segment.id,
-            submission_id=segment.submissionId,
-            text=segment.text,
-            start_index=segment.startIndex,
-            end_index=segment.endIndex,
-        ).save()
+        with get_db() as db:
+            model = DBTextBlock(
+                id=segment.id,
+                submission_id=segment.submissionId,
+                text=segment.text,
+                start_index=segment.startIndex,
+                end_index=segment.endIndex,
+            )
+            db.merge(model)
+            db.commit()
+
+
+def float_list_to_bytes(floats: List[float]) -> bytes:
+    """Convert a list of floats to a bytes object."""
+    return struct.pack('{}f'.format(len(floats)), *floats)
 
 
 def store_text_clusters(exercise_id: int, clusters: Iterable[cofee_pb2.Cluster]):
     """"""
     for cluster in clusters:
-        distance_matrix = [[entry.value for entry in row] for row in cluster.distanceMatrix]
+        distance_matrix = [entry.value for entry in cluster.distanceMatrix]
         # store text cluster in DB
-        DBTextCluster(
-            id=cluster.id,
-            exercise_id=exercise_id,
-            distance_matrix=distance_matrix,
-        ).save()
+        with get_db() as db:
+            model = DBTextCluster(
+                exercise_id=exercise_id,
+                distance_matrix=float_list_to_bytes(distance_matrix),
+            )
+            print(model)
+            db.merge(model)
+            db.commit()
 
 
 def process_results(clusters: List[cofee_pb2.Cluster], segments: List[cofee_pb2.Segment], exercise_id):
