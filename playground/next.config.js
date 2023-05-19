cd2c5bf5f3c89953c4508caddf7c2fcfd1f330de
Paul Schwind <git@pschwind.de>
2023-05-19 22:44:26 +0200

    add PLAYGROUND_BASE_PATH

--- playground/next.config.js
+++ playground/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath: process.env.PLAYGROUND_BASE_PATH || '',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
