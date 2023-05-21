3f04082124f6bc197190236b2af455ae8b8f28a6
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2023-05-21 13:38:25 +0200

    fix linting

--- module_programming_llm/module_programming_llm/helpers/models.py
+++ module_programming_llm/module_programming_llm/helpers/models.py
@@ -6,14 +6,14 @@ OPENAI_API_TYPE = os.environ.get("OPENAI_API_TYPE")
 
 # Validate environment variables
 if "OPENAI_API_KEY" not in os.environ:
-    raise Exception("OPENAI_API_KEY environment variable not set.")
+    raise EnvironmentError("OPENAI_API_KEY environment variable not set.")
 if OPENAI_API_TYPE == "azure":
     if "OPENAI_API_BASE" not in os.environ:
-        raise Exception("OPENAI_API_TYPE=azure but OPENAI_API_BASE environment variable not set.")
+        raise EnvironmentError("OPENAI_API_TYPE=azure but OPENAI_API_BASE environment variable not set.")
     if "OPENAI_API_VERSION" not in os.environ:
-        raise Exception("OPENAI_API_TYPE=azure but OPENAI_API_VERSION environment variable not set.")
+        raise EnvironmentError("OPENAI_API_TYPE=azure but OPENAI_API_VERSION environment variable not set.")
     if "AZURE_DEPLOYMENT_NAME" not in os.environ:
-        raise Exception("OPENAI_API_TYPE=azure but AZURE_DEPLOYMENT_NAME environment variable not set.")
+        raise EnvironmentError("OPENAI_API_TYPE=azure but AZURE_DEPLOYMENT_NAME environment variable not set.")
     
     AZURE_DEPLOYMENT_NAME = os.environ["AZURE_DEPLOYMENT_NAME"]
  
@@ -27,7 +27,7 @@ if OPENAI_API_TYPE == "azure":
     deployment_ids = [deployment.id for deployment in deployments]
     if AZURE_DEPLOYMENT_NAME not in deployment_ids:
         deployments = [{ "id": deployment.id, "model": deployment.model } for deployment in deployments]
-        raise Exception(f"Deployment id '{AZURE_DEPLOYMENT_NAME}' not found, available deployments: {deployments}")
+        raise EnvironmentError(f"Deployment id '{AZURE_DEPLOYMENT_NAME}' not found, available deployments: {deployments}")
 
     # Initialize azure chat model
     chat = AzureChatOpenAI(deployment_name=AZURE_DEPLOYMENT_NAME, client="", temperature=0)
