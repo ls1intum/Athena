c6c0d7178f82b57ea6799dba51eaa6ed4d5c7b0a
Paul Schwind <git@pschwind.de>
2023-05-21 23:27:58 +0200

    change approach to passing athena url variable

--- playground/src/helpers/base_url.ts
+++ playground/src/helpers/base_url.ts
@@ -1,6 +1,1 @@
-// dynamic lookup to be able to change the url at runtime,
-// see https://nextjs.org/docs/pages/building-your-application/configuring/environment-variables#exposing-environment-variables-to-the-browser
-const athenaDomainFromEnv = process.env["NEXT_PUBLIC_ATHENA_DOMAIN"];
-
-const baseUrl = (athenaDomainFromEnv ? `//${athenaDomainFromEnv}` : 'http://localhost:3000') + '/playground';
-export default baseUrl;
+export default '/playground';
--- playground/src/pages/api/exercises.ts
+++ playground/src/pages/api/exercises.ts
@@ -1,10 +1,11 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
 import type { Exercise } from '@/model/exercise';
 import {getExampleExercises} from "@/pages/api/get_examples";
+import getOriginFromRequest from '@/helpers/origin_from_req';
 
 export default function handler(
     req: NextApiRequest,
     res: NextApiResponse<Exercise[]>
 ) {
-    res.status(200).json(getExampleExercises());
+    res.status(200).json(getExampleExercises(getOriginFromRequest(req)));
 }
