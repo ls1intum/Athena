ae58bcc59641c60acfadc50ca19c61109db6f235
Paul Schwind <git@pschwind.de>
2023-05-21 20:50:19 +0200

    redirect playground locally

--- playground/next.config.js
+++ playground/next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: '/playground',
+  // redirect from / to /playground for convenience when developing locally
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/playground',
+        basePath: false,
+        permanent: false,
+      },
+    ];
+  }
 };
 
 module.exports = nextConfig;
