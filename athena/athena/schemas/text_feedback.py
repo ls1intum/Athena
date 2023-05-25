50da205f54e6f3b45e4c7fc4652c05ce8f91014d
Paul Schwind <git@pschwind.de>
2023-05-25 19:16:35 +0200

    mypy type fixing

--- athena/athena/schemas/text_feedback.py
+++ athena/athena/schemas/text_feedback.py
@@ -6,9 +6,13 @@ class TextFeedback(Feedback):
     def get_start_index(self) -> int:
         """Helper function to get the start index of the feedback from its reference."""
         # reference is a string of the form "start_index-end_index"
+        if self.reference is None:
+            raise ValueError("Feedback does not have a reference.")
         return int(self.reference.split('-')[0])
 
     def get_end_index(self) -> int:
         """Helper function to get the end index of the feedback from its reference."""
         # reference is a string of the form "start_index-end_index"
+        if self.reference is None:
+            raise ValueError("Feedback does not have a reference.")
         return int(self.reference.split('-')[1])
