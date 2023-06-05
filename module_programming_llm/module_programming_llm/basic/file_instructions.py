6fd19fe8bdc2e8d430457a726082920037f0600e
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2023-06-05 10:17:16 +0200

    change to multiline string

--- module_programming_llm/module_programming_llm/basic/file_instructions.py
+++ module_programming_llm/module_programming_llm/basic/file_instructions.py
@@ -20,20 +20,20 @@ def generate_file_grading_instructions(exercise: Exercise):
     file_extension = get_file_extension(exercise.programming_language) or ""
     changed_files = get_diff(src_repo=template_repo, dst_repo=solution_repo, file_path=f"*{file_extension}", name_only=True)
     
-    system_template = (
-        'You are a programming tutor AI at a university tasked with grading and providing feedback to programming homework assignments.\n'
-        '\n'
-        'You receive grading instructions and a list of changed files and respond in the following JSON format, associating each file with its grading instructions:\n'
-        '{{"<filename>": "<file grading instructions>"}}\n'
-    )
-    human_template = (
-        'Grading instructions:\n'
-        '{grading_instructions}\n'
-        'Changed files:\n'
-        '{changed_files}\n'
-        '\n'
-        'JSON response:\n'
-    )
+    system_template = """\
+You are a programming tutor AI at a university tasked with grading and providing feedback to programming homework assignments.
+
+You receive grading instructions and a list of changed files and respond in the following JSON format, associating each file with its grading instructions:
+{{"<filename>": "<file grading instructions>"}}
+"""
+    human_template = """\
+Grading instructions:
+{grading_instructions}
+Changed files:
+{changed_files}
+
+JSON response:
+"""
     
     system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
     human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
@@ -56,20 +56,20 @@ def generate_file_problem_statements(exercise: Exercise):
     file_extension = get_file_extension(exercise.programming_language) or ""
     changed_files = get_diff(src_repo=template_repo, dst_repo=solution_repo, file_path=f"*{file_extension}", name_only=True)
         
-    system_template = (
-        'You are a programming tutor AI at a university tasked with grading and providing feedback to programming homework assignments.\n'
-        '\n'
-        'You receive a overall problem statement and a list of changed files and respond in the following JSON format, associating each file with its file-specific problem statement:\n'
-        '{{"<filename>": "<file problem statement>"}}\n'
-    )
-    human_template = (
-        'Problem statement:\n'
-        '{problem_statement}\n'
-        'Changed files:\n'
-        '{changed_files}\n'
-        '\n'
-        'JSON response:\n'
-    )
+    system_template = """\
+You are a programming tutor AI at a university tasked with grading and providing feedback to programming homework assignments.
+
+You receive a overall problem statement and a list of changed files and respond in the following JSON format, associating each file with its file-specific problem statement:
+{{"<filename>": "<file problem statement>"}}
+"""
+    human_template = """\
+Problem statement:
+{problem_statement}
+Changed files:
+{changed_files}
+
+JSON response:
+"""
     
     system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
     human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
