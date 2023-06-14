c8a20c4fbe32c4cdc53551704864666874b286f0
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2023-06-14 10:01:00 +0200

    add note about data

--- playground/src/components/evaluation_mode/index.tsx
+++ playground/src/components/evaluation_mode/index.tsx
@@ -13,6 +13,9 @@ export default function EvaluationMode({
   return (
     <div className="bg-white rounded-md p-4 mt-8">
       <h1 className="text-2xl font-bold mb-4">Evaluate</h1>
+      <p className="text-gray-500 mb-4">
+        Data in <code className="bg-gray-100 p-1">/data</code> directory will be used for evaluation.
+      </p>
       {(module.type === "text" && (
         <TextEvaluation athenaUrl={athenaUrl} athenaSecret={athenaSecret} module={module} />
       )) || (
