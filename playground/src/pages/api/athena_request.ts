d56c59ce6af4fafa58011ec0bbda743d5cbfe975
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2023-06-14 17:19:04 +0200

    fix request body size limit

--- playground/src/pages/api/athena_request.ts
+++ playground/src/pages/api/athena_request.ts
@@ -1,5 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
+export const config = {
+  api: {
+      bodyParser: {
+          sizeLimit: '25mb'
+      }
+  }
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
