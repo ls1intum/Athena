4662b29ebf4d3800b8b63775ae0440e37aba2438
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2023-06-27 17:54:48 +0200

    add code view

--- playground/src/components/details/exercise_detail/programming.tsx
+++ playground/src/components/details/exercise_detail/programming.tsx
@@ -1,12 +1,8 @@
-import { useEffect, useState } from "react";
-import JSZip from "jszip";
-
 import { ProgrammingExercise } from "@/model/exercise";
 import { Mode } from "@/model/mode";
 
 import Disclosure from "@/components/disclosure";
-import { useFetchAndUnzip } from "@/helpers/fetch_and_unzip";
-import FileTree from "@/components/details/repository/file_tree";
+import CodeView from "@/components/details/code_view";
 
 type ProgrammingExerciseDetailProps = {
   exercise: ProgrammingExercise;
@@ -17,23 +13,16 @@ export default function ProgrammingExerciseDetail({
   exercise,
   mode,
 }: ProgrammingExerciseDetailProps) {
-  // exercise.solution_repository_url -> http://localhost:3000/playground/api/mode/example/exercise/1/data/solution.zip
-  // exercise.template_repository_url -> ...
-  // exercise.tests_repository_url -> ...
-  const solution = useFetchAndUnzip(exercise.solution_repository_url);
-  const template = useFetchAndUnzip(exercise.template_repository_url);
-  const tests = useFetchAndUnzip(exercise.tests_repository_url);
-
   return (
     <>
       <Disclosure title="Template Repository">
-        <FileTree tree={template.tree} />
+        <CodeView repository_url={exercise.template_repository_url} />
       </Disclosure>
       <Disclosure title="Solution Repository">
-        <FileTree tree={solution.tree} />
+        <CodeView repository_url={exercise.solution_repository_url} />
       </Disclosure>
       <Disclosure title="Tests Repository">
-        <FileTree tree={tests.tree} />
+        <CodeView repository_url={exercise.tests_repository_url} />
       </Disclosure>
     </>
   );
