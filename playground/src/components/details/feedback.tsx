b35909be2fd14a9bb43a03e5ed7f1f5b43e0b317
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2023-06-28 18:14:18 +0200

    add feedback and fix get data

---
+++ playground/src/components/details/feedback.tsx
@@ -0,0 +1,17 @@
+import Feedback from "@/model/feedback";
+
+type FeedbackDetailProps = {
+  feedback: Feedback;
+};
+
+export default function FeedbackDetail({ feedback }: FeedbackDetailProps) {
+  return (
+    <div>
+      <h3 className="text-lg font-bold">
+        {feedback.text || "Feedback"}
+      </h3>
+      <pre>{feedback.detail_text}</pre>
+    </div>
+  );
+}
+
--- playground/src/helpers/client/get_data.ts
+++ playground/src/helpers/client/get_data.ts
@@ -1,10 +1,9 @@
 import useSWR from "swr";
-
-import { Exercise } from "@/model/exercise";
 import { Mode } from "@/model/mode";
+import { Exercise } from "@/model/exercise";
 import { Submission } from "@/model/submission";
+import Feedback from "@/model/feedback";
 import baseUrl from "@/helpers/base_url";
-import fetcher from "@/helpers/fetcher";
 
 export function useSubmissions(
   mode: Mode,
@@ -20,14 +19,65 @@ export function useSubmissions(
       }
     },
     ([exerciseId, mode, submissionId]) => {
-      return fetcher(
+      return fetch(
         `${baseUrl}/api/mode/${mode}/exercise/${exerciseId}/submissions`
-      );
+      ).then((res) => {
+        if (submissionId !== undefined) {
+          return res
+            .json()
+            .then((submissions) =>
+              submissions.filter(
+                (submission: Submission) => submission.id === submissionId
+              )
+            );
+        } else {
+          return res.json();
+        }
+      });
     }
   );
 
   return {
     submissions: data as Submission[] | undefined,
+    isLoading,
+    error,
+  };
+}
+
+export function useFeedback(
+  mode: Mode,
+  exercise?: Exercise,
+  submissionId?: number
+) {
+  const { data, error, isLoading } = useSWR(
+    () => {
+      if (exercise === undefined) {
+        return null;
+      } else {
+        return [exercise.id, mode, submissionId];
+      }
+    },
+    ([exerciseId, mode, submissionId]) => {
+      return fetch(
+        `${baseUrl}/api/mode/${mode}/exercise/${exerciseId}/feedbacks`
+      ).then((res) => {
+        if (submissionId !== undefined) {
+          return res
+            .json()
+            .then((feedbacks) =>
+              feedbacks.filter(
+                (feedback: Feedback) => feedback.id === submissionId
+              )
+            );
+        } else {
+          return res.json();
+        }
+      });
+    }
+  );
+
+  return {
+    feedbacks: data as Feedback[] | undefined,
     isLoading,
     error,
   };
