515988d79adcc8064b6df1e1fa40a638aaf777ef
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2023-06-30 13:55:50 +0200

    init more scripts and rearrange deps

--- playground/scripts/artemis_export_eval_data.js
+++ playground/scripts/artemis_export_eval_data.js
@@ -1,61 +1,11 @@
 const mysql = require('mysql2/promise');
-const readline = require('readline');
 const fs = require('fs').promises;
 const path = require('path');
 
-const configPath = path.join(__dirname, '.config.json');
+const { loadDBConfig } = require('./db_config.js');
+
 const evaluationDirPath = path.join(__dirname, '..', 'data', 'evaluation');
-
 const textExercisesExportQueryPath = path.join(__dirname, 'export_text_exercises.sql');
-
-const defaultConfig = {
-  host: '127.0.0.1',
-  user: 'root',
-  password: '',
-  database: 'anonymized_artemis',
-};
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-async function loadConfig() {
-  try {
-    const data = await fs.readFile(configPath, 'utf8');
-    const config = JSON.parse(data);
-    console.log('Previous config found (.config.json): ', config);
-    const answer = await askQuestion('Do you want to use the previous config? (yes/no) ');
-    if (answer.toLowerCase() === 'yes') {
-      return config;
-    }
-  } catch (err) {
-    console.log('No previous config found. Enter new details:');
-  }
-  return await promptForConfig();
-}
-
-function askQuestion(query) {
-  return new Promise((resolve) => rl.question(query, resolve));
-}
-
-async function promptForConfig() {
-  const host = await askQuestion(`Enter host: (default: "${defaultConfig.host}") `) || defaultConfig.host;
-  const user = await askQuestion(`Enter username: (default: "${defaultConfig.user}") `) || defaultConfig.user;
-  const password = await askQuestion(`Enter password: (default: "${defaultConfig.password}") `) || defaultConfig.password;
-  const database = await askQuestion(`Enter database: (default: "${defaultConfig.database}") `) || defaultConfig.database;
-
-  const config = { host, user, password, database };
-
-  try {
-    await fs.writeFile(configPath, JSON.stringify(config));
-    console.log('Config saved to ".config.json".');
-  } catch (err) {
-    console.log('Error saving config: ', err);
-  }
-
-  return config;
-}
 
 async function executeQuery(config) {
   const connection = await mysql.createConnection({
@@ -78,8 +28,8 @@ async function executeQuery(config) {
     throw err;
   } finally {
     connection.end();
-    rl.close();
+    process.exit();
   }
 }
 
-loadConfig().then(executeQuery).catch(console.error);
+loadDBConfig().then(executeQuery).catch(console.error);
---
+++ playground/scripts/db_config.js
@@ -0,0 +1,71 @@
+const fs = require("fs").promises;
+const readline = require("readline");
+const path = require("path");
+
+const configPath = path.join(__dirname, "db_config.json");
+
+const defaultConfig = {
+  host: "127.0.0.1",
+  user: "root",
+  password: "",
+  database: "anonymized_artemis",
+};
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+async function loadDBConfig() {
+  try {
+    const data = await fs.readFile(configPath, "utf8");
+    const config = JSON.parse(data);
+    console.log(`Previous config found (${configPath}): `, config);
+    const answer = await askQuestion(
+      "Do you want to use the previous config? (yes/no) "
+    );
+    if (answer.toLowerCase() === "yes" || answer.toLowerCase() === "y") {
+      return config;
+    }
+  } catch (err) {
+    console.log("No previous config found. Enter new details:");
+  }
+  return await promptForConfig();
+}
+
+function askQuestion(query) {
+  return new Promise((resolve) => rl.question(query, resolve));
+}
+
+async function promptForConfig() {
+  const host =
+    (await askQuestion(`Enter host: (default: "${defaultConfig.host}") `)) ||
+    defaultConfig.host;
+  const user =
+    (await askQuestion(
+      `Enter username: (default: "${defaultConfig.user}") `
+    )) || defaultConfig.user;
+  const password =
+    (await askQuestion(
+      `Enter password: (default: "${defaultConfig.password}") `
+    )) || defaultConfig.password;
+  const database =
+    (await askQuestion(
+      `Enter database: (default: "${defaultConfig.database}") `
+    )) || defaultConfig.database;
+
+  const config = { host, user, password, database };
+
+  try {
+    await fs.writeFile(configPath, JSON.stringify(config));
+    console.log(`Config saved to ${configPath}`);
+  } catch (err) {
+    console.log("Error saving config: ", err);
+  }
+
+  return config;
+}
+
+module.exports = {
+  loadDBConfig,
+};
