54861d02cca5b81e61f18298cc1a81bc5ceca152
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2023-06-30 21:37:43 +0200

    fix export eval data script

--- playground/scripts/artemis_export_eval_data.mjs
+++ playground/scripts/artemis_export_eval_data.mjs
@@ -1,8 +1,10 @@
-const mysql = require('mysql2/promise');
-const fs = require('fs').promises;
-const path = require('path');
+import mysql from 'mysql2/promise';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { loadDBConfig } from './db_config.mjs';
 
-const { loadDBConfig } = require('./db_config.mjs');
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const evaluationDirPath = path.join(__dirname, '..', 'data', 'evaluation');
 const textExercisesExportQueryPath = path.join(__dirname, 'export_text_exercises.sql');
@@ -15,13 +17,13 @@ async function executeQuery(config) {
   console.log('Connected to the database!');
 
   try {
-    const sql = await fs.readFile(textExercisesExportQueryPath, 'utf8');
+    const sql = await fs.promises.readFile(textExercisesExportQueryPath, 'utf8');
     const [results] = await connection.query(sql);
     const [exercises] = results.slice(-1);
     exercises.forEach(({ exercise_data }) => {
       const { id } = exercise_data;
       const filePath = path.join(evaluationDirPath, `exercise-${id}.json`);
-      fs.writeFile(filePath, JSON.stringify(exercise_data, null, 2));
+      fs.promises.writeFile(filePath, JSON.stringify(exercise_data, null, 2));
     });
     console.log(`Exported ${exercises.length} exercises to ${evaluationDirPath}`);
   } catch (err) {
