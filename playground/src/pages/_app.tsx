28ce8c6e07b87c782a829bbce45f838fc7e4ae0a
Paul Schwind <git@pschwind.de>
2023-07-04 16:57:22 +0200

    attempt: show branch name in footer of playground

--- playground/src/pages/_app.tsx
+++ playground/src/pages/_app.tsx
@@ -10,6 +10,18 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/logo.png" sizes="any" />
       </Head>
       <Component {...pageProps} />
+      <footer className="p-4 text-white border-t border-opacity-10 text-xs">
+        Branch:&nbsp;
+        {
+          process.env.NEXT_PUBLIC_ATHENA_BRANCH ? (
+            <a href={`https://github.com/ls1intum/Athena/tree/${process.env.ATHENA_BRANCH}`} target="_blank">
+              {process.env.NEXT_PUBLIC_ATHENA_BRANCH}
+            </a>
+          ) : (
+            <span>local</span>
+          )
+        }
+      </footer>
     </>
   );
 }
