3e008362a870a44605b69d0382a05549c2441aec
Paul Schwind <git@pschwind.de>
2023-07-09 20:58:58 +0200

    add text submission language field to DB

--- athena/athena/models/db_text_submission.py
+++ athena/athena/models/db_text_submission.py
@@ -8,6 +8,7 @@ from .db_submission import DBSubmission
 class DBTextSubmission(DBSubmission, Base):
     __tablename__ = "text_submissions"
     text: str = Column(String, nullable=False)  # type: ignore
+    language: str = Column(String, nullable=True)  # type: ignore
 
     exercise_id = Column(Integer, ForeignKey("text_exercises.id", ondelete="CASCADE"), index=True)
 
