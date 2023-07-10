acee86107745c0e37244b701576b856dc60a8fbf
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2023-07-11 00:19:06 +0200

    add cool stuff

--- playground/src/components/module_config/module_llm.tsx
+++ playground/src/components/module_config/module_llm.tsx
@@ -4,6 +4,10 @@ import { ModuleConfigProps } from ".";
 import Form, { getUISchema } from "@/components/form";
 import { UIOptionsType, getDefaultFormState, WidgetProps } from "@rjsf/utils";
 import TextareaAutosize from "react-textarea-autosize";
+import { Editor, Monaco } from "@monaco-editor/react";
+import { useState } from "react";
+import { editor } from "monaco-editor";
+
 
 const AutoResizingTextAreaWidget = ({
   id,
@@ -28,18 +32,66 @@ const AutoResizingTextAreaWidget = ({
     onFocus(id, e.target.value);
   };
 
+  const [height, setHeight] = useState(100);
+
+  const handleEditorDidMount = (editor: editor.IStandaloneCodeEditor, monaco: Monaco) => {
+    editor.onDidContentSizeChange(() => {
+      setHeight(Math.min(1000, editor.getContentHeight()));
+      editor.layout();
+    });
+
+    monaco.languages.register({ id: 'placeholder' });
+    monaco.languages.setMonarchTokensProvider('placeholder', {
+      tokenizer: {
+        root: [
+          [/{\w*}/, 'variable']
+        ]
+      }
+    });
+    monaco.editor.defineTheme('my-theme', {
+      base: 'vs',
+      inherit: true,
+      rules: [
+        { token: 'variable', foreground: '0000FF' }
+      ],
+      colors: {},
+    });
+    monaco.editor.setTheme('my-theme');
+  };
+
   return (
-    <TextareaAutosize
-      id={id}
-      required={required}
-      value={value}
-      disabled={disabled || readonly}
-      autoFocus={autofocus}
-      onChange={handleOnChange}
-      onBlur={handleOnBlur}
-      onFocus={handleOnFocus}
-      className="form-control" // Make sure this matches the class your form uses
-    />
+    <div className="border border-gray-300 rounded my-2 py-2">
+      <div style={{ height }}>
+        <Editor
+          options={{ 
+            scrollBeyondLastLine: false,
+            automaticLayout: true, 
+            scrollbar: {
+              vertical: "hidden",
+              horizontal: "hidden",
+              alwaysConsumeMouseWheel: false,
+            },
+            minimap: {
+              enabled: false,
+            },
+          }}
+          value={value}
+          onMount={handleEditorDidMount}
+          defaultLanguage="placeholder"
+        />
+      </div>
+    </div>
+    // <TextareaAutosize
+    //   id={id}
+    //   required={required}
+    //   value={value}
+    //   disabled={disabled || readonly}
+    //   autoFocus={autofocus}
+    //   onChange={handleOnChange}
+    //   onBlur={handleOnBlur}
+    //   onFocus={handleOnFocus}
+    //   className="form-control" // Make sure this matches the class your form uses
+    // />
   );
 };
 
