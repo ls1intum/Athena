d67dfa44f8737c3d89efa9135c10c2dbb048db45
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2023-07-16 21:17:49 +0200

    handle error earlier

--- playground/scripts/artemis/export_materials_and_submissions.mjs
+++ playground/scripts/artemis/export_materials_and_submissions.mjs
@@ -63,51 +63,51 @@ async function downloadMaterial(exerciseId) {
     }
   );
 
-  if (response.ok) {
-    console.log(`Downloading exercise ${exerciseId}'s material`);
-    const data = await response.arrayBuffer();
-    const materialZip = new JSZip();
-    const materialData = await materialZip.loadAsync(data);
-    const exercisePath = path.join(evaluationOutputDirPath, `exercise-${exerciseId}`);
-
-    const zipFile = Object.keys(materialData.files).find((file) => file.endsWith(".zip"));
-
-    const zip = await materialData.files[zipFile].async("nodebuffer");
-    const materialZipData = await JSZip.loadAsync(zip);
-
-    const writeZip = async (kind) => {
-      const zipFile = Object.keys(materialZipData.files).find((file) =>
-        file.endsWith(`-${kind}.zip`)
-      );
-      if (zipFile) {
-        const zip = await materialZipData.files[zipFile].async("nodebuffer");
-        const unzipPath = path.join(
-          exercisePath,
-          kind === "exercise" ? "template" : kind
-        );
-
-        const zipData = await JSZip.loadAsync(zip);
-        const zipFiles = Object.keys(zipData.files);
-        await Promise.all(
-          zipFiles.map(async (file) => {
-            if (!zipData.files[file].dir) {
-              const data = await zipData.files[file].async("nodebuffer");
-              const filePath = path.join(unzipPath, file);
-              await fs.promises.mkdir(path.dirname(filePath), {
-                recursive: true,
-              });
-              await fs.promises.writeFile(filePath, data);
-            }
-          })
-        );
-      }
-    };
-
-    await Promise.all(["exercise", "solution", "tests"].map(writeZip));
-  } else {
+  if (!response.ok) {
     console.error(`Error downloading exercise ${exerciseId}'s material`);
     process.exit(1);
   }
+
+  console.log(`Downloading exercise ${exerciseId}'s material`);
+  const data = await response.arrayBuffer();
+  const materialZip = new JSZip();
+  const materialData = await materialZip.loadAsync(data);
+  const exercisePath = path.join(evaluationOutputDirPath, `exercise-${exerciseId}`);
+
+  const zipFile = Object.keys(materialData.files).find((file) => file.endsWith(".zip"));
+
+  const zip = await materialData.files[zipFile].async("nodebuffer");
+  const materialZipData = await JSZip.loadAsync(zip);
+
+  const writeZip = async (kind) => {
+    const zipFile = Object.keys(materialZipData.files).find((file) =>
+      file.endsWith(`-${kind}.zip`)
+    );
+    if (zipFile) {
+      const zip = await materialZipData.files[zipFile].async("nodebuffer");
+      const unzipPath = path.join(
+        exercisePath,
+        kind === "exercise" ? "template" : kind
+      );
+
+      const zipData = await JSZip.loadAsync(zip);
+      const zipFiles = Object.keys(zipData.files);
+      await Promise.all(
+        zipFiles.map(async (file) => {
+          if (!zipData.files[file].dir) {
+            const data = await zipData.files[file].async("nodebuffer");
+            const filePath = path.join(unzipPath, file);
+            await fs.promises.mkdir(path.dirname(filePath), {
+              recursive: true,
+            });
+            await fs.promises.writeFile(filePath, data);
+          }
+        })
+      );
+    }
+  };
+
+  await Promise.all(["exercise", "solution", "tests"].map(writeZip));
 };
 
 async function downloadSubmissions(exerciseId) {
@@ -126,44 +126,44 @@ async function downloadSubmissions(exerciseId) {
     }
   );
 
-  if (response.ok) {
-    console.log(`Downloaded exercise ${exerciseId}'s submissions`);
-    const data = await response.arrayBuffer();
-    const submissionsZip = new JSZip();
-    const submissionsData = await submissionsZip.loadAsync(data);
-    const submissionsPath = path.join(evaluationOutputDirPath, `exercise-${exerciseId}`, "submissions");
-
-    const submissionZipFiles = Object.keys(submissionsData.files);
-    await Promise.all(
-      submissionZipFiles.map(async (file) => {
-        if (!submissionsData.files[file].dir) {
-          const submissionId = file.match(
-            /-(\d+)-student-submission.git.zip$/
-          )[1];
-          const zip = await submissionsData.files[file].async("nodebuffer");
-          const unzipPath = path.join(submissionsPath, submissionId);
-
-          const zipData = await JSZip.loadAsync(zip);
-          const zipFiles = Object.keys(zipData.files);
-          await Promise.all(
-            zipFiles.map(async (file) => {
-              if (!zipData.files[file].dir) {
-                const data = await zipData.files[file].async("nodebuffer");
-                const filePath = path.join(unzipPath, file);
-                await fs.promises.mkdir(path.dirname(filePath), {
-                  recursive: true,
-                });
-                await fs.promises.writeFile(filePath, data);
-              }
-            })
-          );
-        }
-      })
-    );
-  } else {
+  if (!response.ok) {
     console.error(`Error downloading exercise ${exerciseId}'s submissions`);
     process.exit(1);
   }
+  
+  console.log(`Downloaded exercise ${exerciseId}'s submissions`);
+  const data = await response.arrayBuffer();
+  const submissionsZip = new JSZip();
+  const submissionsData = await submissionsZip.loadAsync(data);
+  const submissionsPath = path.join(evaluationOutputDirPath, `exercise-${exerciseId}`, "submissions");
+
+  const submissionZipFiles = Object.keys(submissionsData.files);
+  await Promise.all(
+    submissionZipFiles.map(async (file) => {
+      if (!submissionsData.files[file].dir) {
+        const submissionId = file.match(
+          /-(\d+)-student-submission.git.zip$/
+        )[1];
+        const zip = await submissionsData.files[file].async("nodebuffer");
+        const unzipPath = path.join(submissionsPath, submissionId);
+
+        const zipData = await JSZip.loadAsync(zip);
+        const zipFiles = Object.keys(zipData.files);
+        await Promise.all(
+          zipFiles.map(async (file) => {
+            if (!zipData.files[file].dir) {
+              const data = await zipData.files[file].async("nodebuffer");
+              const filePath = path.join(unzipPath, file);
+              await fs.promises.mkdir(path.dirname(filePath), {
+                recursive: true,
+              });
+              await fs.promises.writeFile(filePath, data);
+            }
+          })
+        );
+      }
+    })
+  );
 };
 
 async function download(exerciseId) {
