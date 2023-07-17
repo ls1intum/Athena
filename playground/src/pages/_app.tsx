b752ec460ec8503f6a1fbd4549e719209617efd7
Paul Schwind <git@pschwind.de>
2023-07-17 13:53:01 +0200

    improve passed info even more

--- playground/src/pages/_app.tsx
+++ playground/src/pages/_app.tsx
@@ -12,19 +12,26 @@ export default function App({ Component, pageProps }: AppProps) {
       <Component {...pageProps} />
       <footer className="p-4 text-white border-t border-opacity-10 text-xs">
         {
-          process.env.NEXT_PUBLIC_ATHENA_COMMIT_SHA ? <>
-            <a href={`https://github.com/ls1intum/Athena/pull/${process.env.NEXT_PUBLIC_ATHENA_PR_NUMBER}`} target="_blank" className="hover:underline">
-              PR&nbsp;#{process.env.NEXT_PUBLIC_ATHENA_PR_NUMBER}:&nbsp;
-              {process.env.NEXT_PUBLIC_ATHENA_PR_TITLE}
+          process.env.NEXT_PUBLIC_ATHENA_IS_DEVELOP ? <>
+            <span>develop</span>&nbsp;-&nbsp;
+            <a href={`https://github.com/ls1intum/Athena/commit/${process.env.NEXT_PUBLIC_ATHENA_COMMIT_SHA}`} target="_blank" className="hover:underline">
+              Commit:&nbsp;{(process.env.NEXT_PUBLIC_ATHENA_COMMIT_SHA ?? '').slice(0, 7)}
             </a>&nbsp;-&nbsp;
-            <a href={`https://github.com/ls1intum/Athena/pull/${process.env.NEXT_PUBLIC_ATHENA_PR_NUMBER}/commits/${process.env.NEXT_PUBLIC_ATHENA_COMMIT_SHA}`} target="_blank" className="hover:underline">
-              Commit:&nbsp;{process.env.NEXT_PUBLIC_ATHENA_COMMIT_SHA.slice(0, 7)}
-            </a>&nbsp;-&nbsp;
-            <span>
-              Last update:&nbsp;{process.env.NEXT_PUBLIC_ATHENA_LAST_UPDATE}
-            </span>
-          </> : (
-            <span>Local build</span>
+            </> : (
+            process.env.NEXT_PUBLIC_ATHENA_COMMIT_SHA ? <>
+              <a href={`https://github.com/ls1intum/Athena/pull/${process.env.NEXT_PUBLIC_ATHENA_PR_NUMBER}`} target="_blank" className="hover:underline">
+                PR&nbsp;#{process.env.NEXT_PUBLIC_ATHENA_PR_NUMBER}:&nbsp;
+                {process.env.NEXT_PUBLIC_ATHENA_PR_TITLE}
+              </a>&nbsp;-&nbsp;
+              <a href={`https://github.com/ls1intum/Athena/pull/${process.env.NEXT_PUBLIC_ATHENA_PR_NUMBER}/commits/${process.env.NEXT_PUBLIC_ATHENA_COMMIT_SHA}`} target="_blank" className="hover:underline">
+                Commit:&nbsp;{(process.env.NEXT_PUBLIC_ATHENA_COMMIT_SHA ?? '').slice(0, 7)}
+              </a>&nbsp;-&nbsp;
+              <span>
+                Last update:&nbsp;{process.env.NEXT_PUBLIC_ATHENA_PR_LAST_UPDATE}
+              </span>
+            </> : (
+              <span>Local build</span>
+            )
           )
         }
       </footer>
