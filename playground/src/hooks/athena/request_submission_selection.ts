8738f01ccff2a183787a539ba64d50c0b19715a1
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2023-07-17 20:16:57 +0200

    add useRequestSubmissionSelection

---
+++ playground/src/hooks/athena/request_submission_selection.ts
@@ -0,0 +1,40 @@
+import { UseMutationOptions, useMutation } from "react-query";
+import { AthenaError, useAthenaFetcher } from "@/hooks/athena_fetcher";
+import { ModuleMeta } from "@/model/health_response";
+import { Exercise } from "@/model/exercise";
+import { Submission } from "@/model/submission";
+import ModuleResponse from "@/model/module_response";
+
+
+/**
+ * Request submission selection for an exercise from an Athena module.
+ *
+ * @example
+ * const { data, isLoading, error, mutate } = useRequestSubmissionSelection(module);
+ * mutate({ exercise, submissions });
+ * 
+ * @param module The module to request the submission selection from.
+ * @param options The react-query options.
+ */
+export default function useRequestSubmissionSelection(
+  module?: ModuleMeta,
+  options: Omit<
+    UseMutationOptions<ModuleResponse, AthenaError, { exercise: Exercise; submissions: Submission[] }>,
+    "mutationFn"
+  > = {}
+) {
+  const athenaFetcher = useAthenaFetcher(module);
+  return useMutation({
+    mutationFn: async ({
+      exercise,
+      submissions,
+    }) => {
+      if (athenaFetcher === undefined) {
+        throw new AthenaError("No module set.", 0, undefined);
+      }
+      const submissionIds = submissions.map((submission) => submission.id)
+      return await athenaFetcher("/select_submission", { exercise, submission_ids: submissionIds });
+    },
+    ...options,
+  });
+}
