de705a8f94b422233552a4efd92895fe1c7d4d6e
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2023-07-17 23:00:02 +0200

    replace swr

--- playground/src/components/health.tsx
+++ playground/src/components/health.tsx
@@ -1,14 +1,14 @@
-import useSWR from "swr";
+import { useQuery } from "react-query";
 import fetcher from "@/helpers/fetcher";
 import HealthResponse from "@/model/health_response";
 import baseUrl from "@/helpers/base_url";
 
 export default function Health({ url }: { url: string }) {
   // show red and green dots for module health
-  const { data, error } = useSWR<HealthResponse>(
-    `${baseUrl}/api/health?url=${encodeURIComponent(url)}`,
-    fetcher
-  );
+  const { data, error } = useQuery({
+    queryKey: ["health", url],
+    queryFn: async () => await fetcher(`${baseUrl}/api/health?url=${encodeURIComponent(url)}`) as HealthResponse,
+  })
   if (error) return <div className="text-red-500 text-sm">Failed to load</div>;
   if (!data) return <div className="text-gray-500 text-sm">Loading...</div>;
   return (
