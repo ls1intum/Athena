2134b677a90a3873da3553058a1673dad81c6b77
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2023-07-18 11:53:57 +0200

    add evaluation mode page

--- playground/src/pages/index.tsx
+++ playground/src/pages/index.tsx
@@ -1,17 +1,17 @@
 import React from "react";
+import { useBaseInfo } from "@/hooks/base_info_context";
 import BaseInfoHeader from "@/components/base_info_header";
 import ModuleRequests from "@/components/view_mode/module_requests";
-import { useBaseInfo } from "@/hooks/base_info_context";
-
+import EvaluationMode from "@/components/view_mode/evaluation_mode";
 
 export default function Playground() {
-  const { module } = useBaseInfo();
+  const { module, viewMode } = useBaseInfo();
 
   return (
     <main className="flex min-h-screen flex-col p-24">
       <h1 className="text-6xl font-bold text-white mb-8">Playground</h1>
       <BaseInfoHeader />
-      {module && (<ModuleRequests module={module} />)}
+      {module && (viewMode === "module_requests" ? <ModuleRequests module={module} /> : <EvaluationMode />)}
     </main>
   );
 }
