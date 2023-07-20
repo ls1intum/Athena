bdeb62c53913683b52020a32f691a3a8a5d3be6d
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2023-07-20 20:47:32 +0200

    improve naming

--- playground/src/model/feedback.ts
+++ playground/src/model/feedback.ts
@@ -14,3 +14,29 @@ type Feedback = {
 };
 
 export default Feedback;
+
+
+type FeedbackBase = {
+  id: number;
+  detail_text: string;
+  text: string;
+  credits: number;
+  meta: {
+    [key: string]: any;
+  };
+  exercise_id: number;
+  submission_id: number;
+};
+
+export type TextFeedback = FeedbackBase & {
+  index_start?: number;
+  index_end?: number;
+};
+
+export type ProgrammingFeedback = FeedbackBase & {
+  file_path?: string;
+  line_start?: number;
+  line_end?: number;
+};
+
+export type Feedback = TextFeedback | ProgrammingFeedback;
