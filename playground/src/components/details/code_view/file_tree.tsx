3f9dd67b74163928825458f60ea0f77bd0e77a9d
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2023-07-22 10:17:26 +0200

    improe file tree

--- playground/src/components/details/code_view/file_tree.tsx
+++ playground/src/components/details/code_view/file_tree.tsx
@@ -1,20 +1,48 @@
 import { FileTree } from "@/helpers/fetch_and_unzip";
 
-export default function FileTree({ tree }: { tree: FileTree[] }) {
+type FileTreeProps = {
+  tree: FileTree[];
+  selectedFile?: string;
+  onSelectFile: (path: string) => void;
+  level?: number;
+};
+
+export default function FileTree({
+  tree,
+  selectedFile,
+  onSelectFile,
+  level = 0,
+}: FileTreeProps) {
   return (
     <ul>
-      {tree.map((file, index) => (
-        <li key={index} className="ml-2">
-          {file.dir ? (
-            <>
-              {file.dirname}
-              {file.children && <FileTree tree={file.children} />}
-            </>
-          ) : (
-            file.filename
-          )}
-        </li>
-      ))}
+      {tree.map((file, index) =>
+        file.dir ? (
+          <li key={index} className={level === 0 ? "pl-2" : ""}>
+            {file.dirname}/
+            {file.children && (
+              <FileTree
+                tree={file.children}
+                selectedFile={selectedFile}
+                onSelectFile={onSelectFile}
+                level={level + 1}
+              />
+            )}
+          </li>
+        ) : (
+          <li key={index}>
+            <button
+              className={`${
+                selectedFile === file.path
+                  ? "text-primary-500"
+                  : "text-gray-500"
+              } hover:text-primary-500 hover:bg-gray-100 rounded-md w-full flex items-center justify-start pl-2`}
+              onClick={() => onSelectFile(file.path)}
+            >
+              {file.filename}
+            </button>
+          </li>
+        )
+      )}
     </ul>
   );
 }
--- playground/src/components/details/code_view/index.tsx
+++ playground/src/components/details/code_view/index.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { Allotment } from "allotment";
+
 import { useFetchAndUnzip } from "@/helpers/fetch_and_unzip";
 
 import FileTree from "./file_tree";
@@ -8,6 +11,17 @@ type CodeViewProps = {
 
 export default function CodeView({ repository_url }: CodeViewProps) {
   const repository = useFetchAndUnzip(repository_url);
+  const [selectedFile, setSelectedFile] = useState<string | undefined>(undefined);
 
-  return <FileTree tree={repository.tree} />;
+  return <Allotment vertical={false} defaultSizes={[1, 4]}>
+  <Allotment.Pane preferredSize={"20%"}>
+    <div className="h-full overflow-y-auto mr-2">
+      <FileTree
+        tree={repository.tree}
+        selectedFile={selectedFile}
+        onSelectFile={setSelectedFile}
+      />
+    </div>
+  </Allotment.Pane>
+  </Allotment>
 }
