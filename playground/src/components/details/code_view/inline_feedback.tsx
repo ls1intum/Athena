c3711b12033414cbb63a3209be4644917fb347b0
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2023-07-22 19:21:57 +0200

    add basic layout

--- playground/src/components/details/code_view/inline_feedback.tsx
+++ playground/src/components/details/code_view/inline_feedback.tsx
@@ -1,14 +1,27 @@
-import { Feedback } from "@/model/feedback";
+import { Feedback, formatReference } from "@/model/feedback";
 
 type InlineFeedbackProps = {
   feedback: Feedback;
   onFeedbackChange?: (feedback: Feedback) => void;
 };
 
-export default function InlineFeedback({ feedback, onFeedbackChange }: InlineFeedbackProps) {
+export default function InlineFeedback({
+  feedback,
+  onFeedbackChange,
+}: InlineFeedbackProps) {
   return (
-    <div>
-      Text
+    <div className="m-2 border border-gray-300 rounded-lg">
+      <div className="flex items-center justify-between px-4 py-2 border-b border-gray-300 text-xs">
+        Suggestion on {formatReference(feedback)}
+      </div>
+      <div className="flex items-center justify-start">
+        <div>Points</div>
+        <div>
+          <div>Title</div>
+          <div>Description</div>
+        </div>
+      </div>
+      <div>Controls</div>
     </div>
   );
 }
