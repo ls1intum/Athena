9887047c8a337c32c58132ae7811888bef8430bb
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2023-07-22 10:33:10 +0200

    add working checkpoint

--- playground/src/components/details/code_view/file_editor.tsx
+++ playground/src/components/details/code_view/file_editor.tsx
@@ -1,4 +1,9 @@
 import { Feedback } from "@/model/feedback";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { Monaco, Editor, useMonaco } from "@monaco-editor/react";
+import { editor } from "monaco-editor";
+import * as portals from 'react-reverse-portal';
+import { createRoot } from "react-dom/client";
 
 type FileEditorProps = {
   content: string;
@@ -13,5 +18,86 @@ export default function FileEditor({
   feedbacks,
   onFeedbackChange,
 }: FileEditorProps) {
-  return <div>FileEditor</div>;
+  const editorRef = useRef<editor.IStandaloneCodeEditor>();
+  const monaco = useMonaco();
+
+  const [count, setCount] = useState<number>(0);
+  const portalNode = useMemo(() => portals.createHtmlPortalNode(), []);
+
+  const MyComponent = () => <div>
+    My first view zone {count}
+    <button onClick={() => {
+      setCount(count + 1)
+      console.log("Click")
+    }}>Click me</button>
+  </div>;
+  
+  useEffect(() => {
+    if (monaco) {
+      console.log('here is the monaco instance:', monaco);
+      console.log('Models', monaco.editor.getModels())
+      const path = monaco.Uri.parse(filePath || "")
+      monaco.editor.getModel(path)?.dispose()
+      console.log("Create model")
+      const model = monaco.editor.createModel(content, undefined, path)
+      editorRef.current?.setModel(model);
+    }
+  }, [monaco, filePath, content]);
+
+  const handleEditorDidMount = (editor: editor.IStandaloneCodeEditor, monaco: Monaco) => {
+    editorRef.current = editor;
+    
+    const overlayDom = document.createElement("div");
+    overlayDom.id = 'overlayId';
+    overlayDom.style.width = '100%';
+    // overlayDom.style.background = '#0000ff';
+    const root = createRoot(overlayDom);
+    root.render(<portals.OutPortal node={portalNode} />);
+    
+    var overlayWidget = {
+      getId: () => "my.overlay.widget",
+      getDomNode: () => overlayDom,
+      getPosition: () => null,
+    };
+    editor.addOverlayWidget(overlayWidget);
+
+    let zoneNode = document.createElement('div');
+    zoneNode.id = "zoneId";
+
+    editorRef.current?.changeViewZones(function (changeAccessor) {
+      changeAccessor.addZone({
+        afterLineNumber: 3,
+        heightInLines: 3,
+        domNode: zoneNode,
+        onDomNodeTop: top => {
+          overlayDom.style.top = top + "px";
+        },
+        onComputedHeight: height => {
+          overlayDom.style.height = height + "px";
+        }
+      });
+    });
+  }
+
+  return <div className="h-[50vh]">
+  <Editor
+    options={{
+      automaticLayout: true,
+      scrollbar: {
+        vertical: "hidden",
+        horizontal: "hidden",
+      },
+      minimap: {
+        enabled: false,
+      },
+      readOnly: true,
+    }}
+    value={content}
+    path={filePath}
+    defaultValue="Please select a file"
+    onMount={handleEditorDidMount}
+    // onChange={(value) => onChange(value)}
+  />
+  <portals.InPortal node={portalNode}><MyComponent /></portals.InPortal>
+</div>;
 }
