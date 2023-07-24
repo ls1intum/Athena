b2c667dd1763353ea7efed9998e07257b947be8f
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2023-07-24 14:16:01 +0200

    change editable inline feedback

--- playground/src/components/details/code_view/inline_feedback.tsx
+++ playground/src/components/details/code_view/inline_feedback.tsx
@@ -1,6 +1,7 @@
 import { Feedback, formatReference } from "@/model/feedback";
 import { useState } from "react";
 import { twMerge } from "tailwind-merge";
+import TextareaAutosize from "react-textarea-autosize";
 
 type InlineFeedbackProps = {
   feedback: Feedback;
@@ -26,52 +27,78 @@ export default function InlineFeedback({
       <div className="flex items-center justify-start px-4 py-2 border-b border-gray-300 text-xs text-gray-600">
         Feedback on {formatReference(feedback)}
       </div>
-      <div className="flex justify-start items-center space-x-2 px-4 py-2">
-        {isEditing ? (<>
+      <div className="flex justify-start items-start space-x-2 px-4 py-2">
+        {isEditing ? (
           <input
-              className={twMerge(
-                "font-medium rounded pl-2.5 py-0.5 w-16",
-                feedback.credits < 0
-                  ? "bg-red-100 text-red-800"
-                  : feedback.credits > 0
-                  ? "bg-green-100 text-green-800"
-                  : "bg-gray-100 text-gray-800"
-              )}
-              type="number"
-              step="0.5"
-              value={feedback.credits}
-              onChange={(e) => {
-                onFeedbackChange({
-                  ...feedback,
-                  credits: e.target.value == "" ? 0 : parseFloat(e.target.value),
-                })
-              }
-              }
-            />
-        </>) : (<div
-          className={twMerge(
-            "font-medium rounded px-2.5 py-0.5",
-            feedback.credits < 0
-              ? "bg-red-100 text-red-800"
-              : feedback.credits > 0
-              ? "bg-green-100 text-green-800"
-              : "bg-gray-100 text-gray-800"
-          )}
-        >
-          {feedback.credits}&nbsp;P
-        </div>)}
+            className={twMerge(
+              "font-medium rounded pl-2.5 py-0.5 w-16 mt-2 border border-gray-300",
+              feedback.credits < 0
+                ? "bg-red-100 text-red-800"
+                : feedback.credits > 0
+                ? "bg-green-100 text-green-800"
+                : "bg-gray-100 text-gray-800"
+            )}
+            type="number"
+            step="0.5"
+            value={feedback.credits}
+            onChange={(e) => {
+              onFeedbackChange({
+                ...feedback,
+                credits: e.target.value == "" ? 0 : parseFloat(e.target.value),
+              });
+            }}
+          />
+        ) : (
+          <div
+            className={twMerge(
+              "font-medium rounded px-2.5 py-0.5 mt-2",
+              feedback.credits < 0
+                ? "bg-red-100 text-red-800"
+                : feedback.credits > 0
+                ? "bg-green-100 text-green-800"
+                : "bg-gray-100 text-gray-800"
+            )}
+          >
+            {feedback.credits}&nbsp;P
+          </div>
+        )}
         <div className="w-full">
-          <div className="flex items-center space-x-1">
-            <span className="font-semibold">
-              {feedback.title ? feedback.title : <i>Missing title</i>}
-            </span>
+          <div className="flex items-center justify-between space-x-1">
+            {isEditing ? (
+              <input
+                className="font-semibold w-full border border-gray-300 rounded p-1"
+                value={feedback.title}
+                placeholder="Title..."
+                onChange={(e) =>
+                  onFeedbackChange({ ...feedback, title: e.target.value })
+                }
+              />
+            ) : (
+              <span className="font-semibold">
+                {feedback.title ? feedback.title : <i>Missing title</i>}
+              </span>
+            )}
             <span className="text-xs text-gray-500 rounded-full px-2 py-0.5 bg-gray-100">
               Suggestion
             </span>
           </div>
           <div>
-            {feedback.description ? (
-              feedback.description
+            {isEditing ? (
+              <TextareaAutosize
+                className="w-full border border-gray-300 rounded p-1 mt-1"
+                value={feedback.description}
+                placeholder="Description..."
+                onChange={(e) =>
+                  onFeedbackChange({
+                    ...feedback,
+                    description: e.target.value,
+                  })
+                }
+              />
+            ) : feedback.description ? (
+              <span className="whitespace-pre-wrap">
+                {feedback.description}
+              </span>
             ) : (
               <i>Missing description</i>
             )}
