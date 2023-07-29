84216f87fb56f7505b104e98bd8e97ae6e49af6f
Paul Schwind <git@pschwind.de>
2023-07-29 03:23:48 +0200

    simplify changed files getting

--- .github/scripts/images-to-build.sh
+++ .github/scripts/images-to-build.sh
@@ -15,7 +15,7 @@ DIRS=()
 
 # Get a list of all files changed in the current pull request
 git fetch origin $GITHUB_BASE_REF # Make sure we have the older ref as well
-CHANGED_FILES=$(git diff --name-only HEAD $(git merge-base HEAD $LAST_REF_BEFORE_PUSH) 2>&1)
+CHANGED_FILES=$(git diff --name-only HEAD $LAST_REF_BEFORE_PUSH 2>&1)
 
 if [ $? -ne 0 ]; then
     # The command failed. Print the error message.
