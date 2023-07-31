394311dcdedfe90552ede1c1cb28f0ea6cd5940d
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2023-07-31 13:24:47 +0200

    add evaluation mode

---
+++ playground/src/components/view_mode/evaluation_mode/index.tsx
@@ -0,0 +1,7 @@
+export default function EvaluationMode() {
+  return (
+    <>
+      <h2 className="text-4xl font-bold text-white mb-4">Evaluation Mode</h2>
+    </>
+  );
+}
--- playground/src/pages/index.tsx
+++ playground/src/pages/index.tsx
@@ -1,13 +1,19 @@
 import React from "react";
+
+import { useBaseInfo } from "@/hooks/base_info_context";
 import BaseInfoHeader from "@/components/base_info_header";
 import ModuleRequests from "@/components/view_mode/module_requests";
+import EvaluationMode from "@/components/view_mode/evaluation_mode";
 
 export default function Playground() {
+  const { viewMode } = useBaseInfo();
+
   return (
     <main className="flex min-h-screen flex-col p-24">
       <h1 className="text-6xl font-bold text-white mb-8">Playground</h1>
       <BaseInfoHeader />
-      <ModuleRequests />
+      {viewMode === "module_requests" && <ModuleRequests />}
+      {viewMode === "evaluation_mode" && <EvaluationMode />}
     </main>
   );
 }
