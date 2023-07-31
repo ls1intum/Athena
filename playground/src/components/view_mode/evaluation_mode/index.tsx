8d1ae0ef4211ab801630fd7b32a9a1d006c36a72
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2023-07-31 19:08:54 +0200

    add placeholders

--- playground/src/components/view_mode/evaluation_mode/index.tsx
+++ playground/src/components/view_mode/evaluation_mode/index.tsx
@@ -2,6 +2,15 @@ export default function EvaluationMode() {
   return (
     <>
       <h2 className="text-4xl font-bold text-white mb-4">Evaluation Mode</h2>
+      <div className="bg-white rounded-md p-4 mb-8">
+        <h3 className="text-2xl font-bold mb-4">
+          Experiment
+        </h3>
+        <p>Select Exercise</p>
+        <p>Select mode</p>
+        <p>Select Submissions to use</p>
+        <p>Import / Export</p>
+      </div>
     </>
   );
 }
