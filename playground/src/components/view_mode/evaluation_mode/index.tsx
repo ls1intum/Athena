ab3d4731823c05e519bd287dcf97210860506c38
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2023-08-01 11:58:32 +0200

    add exercise detail

--- playground/src/components/view_mode/evaluation_mode/index.tsx
+++ playground/src/components/view_mode/evaluation_mode/index.tsx
@@ -1,28 +1,35 @@
-import ExerciseTypeSelect from "@/components/selectors/exercise_type_select";
-import useHealth from "@/hooks/health";
 import type { Exercise } from "@/model/exercise";
-import { HealthResponse } from "@/model/health_response";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+import { useBaseInfo } from "@/hooks/base_info_context";
+
+import ExerciseTypeSelect from "@/components/selectors/exercise_type_select";
+import ExerciseSelect from "@/components/selectors/exercise_select";
+import ExerciseDetail from "@/components/details/exercise_detail";
 
 export default function EvaluationMode() {
-  const [exerciseType, setExerciseType] = useState<string | undefined>(undefined); // TODO: useExerciseType
+  const { dataMode } = useBaseInfo();
+  const [exerciseType, setExerciseType] = useState<string | undefined>(undefined);
   const [exercise, setExercise] = useState<Exercise | undefined>(undefined);
-  const { data: healthData, error } = useHealth();
 
-  const healthyModules = (healthResponse: HealthResponse) => {
-    return Object.values(healthResponse.modules).filter((module) => module.healthy);
-  }
+  useEffect(() => setExerciseType(undefined), [dataMode]);
+  useEffect(() => setExercise(undefined), [exerciseType, dataMode]);
 
   return (
     <>
       <h2 className="text-4xl font-bold text-white mb-4">Evaluation Mode</h2>
-      <div className="bg-white rounded-md p-4 mb-8">
-        <h3 className="text-2xl font-bold mb-4">
+      <div className="bg-white rounded-md p-4 mb-8 space-y-2">
+        <h3 className="text-2xl font-bold">
           Experiment
         </h3>
         <ExerciseTypeSelect exerciseType={exerciseType} onChangeExerciseType={setExerciseType} />
-        <p>Select Exercise</p>
+        {exerciseType && (
+          <ExerciseSelect exercise={exercise} exerciseType={exerciseType} onChange={setExercise} />
+        )}
+        {exercise && (
+          <ExerciseDetail exercise={exercise} />
+        )}
         <p>Select mode</p>
         <p>Select Submissions to use</p>
         <p>Import / Export</p>
