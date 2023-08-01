b725c7ae431bec34e037ad527250dcf43386dae6
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2023-08-01 11:46:14 +0200

    add select exercise type

---
+++ playground/src/components/selectors/exercise_type_select.tsx
@@ -0,0 +1,51 @@
+import type { HealthResponse } from "@/model/health_response";
+import useHealth from "@/hooks/health";
+
+export default function ExerciseTypeSelect({
+  exerciseType,
+  onChangeExerciseType: onChangeType,
+}: {
+  exerciseType: string | undefined;
+  onChangeExerciseType: (type: string) => void;
+}) {
+  const { data, error } = useHealth();
+  if (error) return <div className="text-red-500 text-sm">Failed to load</div>;
+  if (!data) return <div className="text-gray-500 text-sm">Loading...</div>;
+
+  const getModuleTypes = (healthResponse: HealthResponse): { healthy: boolean; type: string; }[] => {
+    const types = new Set<string>();
+    Object.values(healthResponse.modules).forEach((module) => {
+      types.add(module.type);
+    });
+
+    return Array.from(types).map((type) => {
+      return {
+        type,
+        healthy: Object.values(healthResponse.modules).filter((module) => module.type === type).some((module) => module.healthy),
+      }
+    });
+  }
+
+  return (
+    <label className="flex flex-col">
+      <span className="text-lg font-bold">Exercise Type</span>
+      <select
+        className="border border-gray-300 rounded-md p-2"
+        value={exerciseType ?? ""}
+        onChange={(e) => onChangeType(e.target.value)}
+      >
+        <option value={""} disabled>
+          Select an exercise type
+        </option>
+        {getModuleTypes(data).map(({ type, healthy }) => {
+          return (
+            <option key={type} value={type} disabled={!healthy}>
+              {type}
+              {healthy ? "" : " (none healthy!)"}
+            </option>
+          );
+        })}
+      </select>
+    </label>
+  );
+}
--- playground/src/components/view_mode/evaluation_mode/index.tsx
+++ playground/src/components/view_mode/evaluation_mode/index.tsx
@@ -1,4 +1,19 @@
+import ExerciseTypeSelect from "@/components/selectors/exercise_type_select";
+import useHealth from "@/hooks/health";
+import type { Exercise } from "@/model/exercise";
+import { HealthResponse } from "@/model/health_response";
+
+import { useState } from "react";
+
 export default function EvaluationMode() {
+  const [exerciseType, setExerciseType] = useState<string | undefined>(undefined); // TODO: useExerciseType
+  const [exercise, setExercise] = useState<Exercise | undefined>(undefined);
+  const { data: healthData, error } = useHealth();
+
+  const healthyModules = (healthResponse: HealthResponse) => {
+    return Object.values(healthResponse.modules).filter((module) => module.healthy);
+  }
+
   return (
     <>
       <h2 className="text-4xl font-bold text-white mb-4">Evaluation Mode</h2>
@@ -6,6 +21,7 @@ export default function EvaluationMode() {
         <h3 className="text-2xl font-bold mb-4">
           Experiment
         </h3>
+        <ExerciseTypeSelect exerciseType={exerciseType} onChangeExerciseType={setExerciseType} />
         <p>Select Exercise</p>
         <p>Select mode</p>
         <p>Select Submissions to use</p>
