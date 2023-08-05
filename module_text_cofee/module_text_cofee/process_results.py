8d33584ab879999e43a2d901ba05d4a117fc9df7
Paul Schwind <git@pschwind.de>
2023-08-05 04:40:03 +0200

    correctly pass cluster IDs for processing

--- module_text_cofee/module_text_cofee/process_results.py
+++ module_text_cofee/module_text_cofee/process_results.py
@@ -31,8 +31,9 @@ def store_text_blocks(segments: List[cofee_pb2.Segment], clusters: List[cofee_pb
             db.commit()
 
 
-def store_text_clusters(exercise_id: int, clusters: Iterable[cofee_pb2.Cluster]):  # type: ignore
-    """Store text clusters in the DB."""
+def store_text_clusters(exercise_id: int, clusters: Iterable[cofee_pb2.Cluster]) -> List[int]:  # type: ignore
+    """Store text clusters in the DB. Returns the cluster IDs."""
+    cluster_ids = []
     for cluster in clusters:
         distance_matrix: List[List[float]] = [[0.0 for _ in range(len(cluster.segments))] for _ in range(len(cluster.segments))]
         for entry in cluster.distanceMatrix:
@@ -45,9 +46,11 @@ def store_text_clusters(exercise_id: int, clusters: Iterable[cofee_pb2.Cluster])
             model.distance_matrix = distance_matrix
             db.merge(model)
             db.commit()
+            cluster_ids.append(model.id)
+    return cluster_ids
 
 
-def connect_text_blocks_to_clusters(clusters: List[cofee_pb2.Cluster]):  # type: ignore
+def connect_text_blocks_to_clusters(clusters: List[cofee_pb2.Cluster], cluster_ids):  # type: ignore
     """
     Connect text blocks to clusters via the cluster_id field.
     Also store the positions of the text blocks in the clusters in the DB, in the same order that Athena-CoFee provides.
@@ -55,12 +58,12 @@ def connect_text_blocks_to_clusters(clusters: List[cofee_pb2.Cluster]):  # type:
     two text blocks, we need to know their positions in the cluster because that's the place where we need to look 
     up the distance in the distance matrix.
     """
-    for cluster in clusters:
+    for cluster, cluster_id in zip(clusters, cluster_ids):
         with get_db() as db:
             for i, segment in enumerate(cluster.segments):
                 model = db.query(DBTextBlock).filter(DBTextBlock.id == segment.id).one()
                 model.position_in_cluster = i
-                model.cluster_id = cluster.id
+                model.cluster_id = cluster_id  # cannot use cluster.id because cluster is an object from Athena and we need the DB ID
                 db.merge(model)
             db.commit()
 
@@ -68,7 +71,7 @@ def connect_text_blocks_to_clusters(clusters: List[cofee_pb2.Cluster]):  # type:
 def process_results(clusters: List[cofee_pb2.Cluster], segments: List[cofee_pb2.Segment], exercise_id):  # type: ignore
     """Processes results coming back from the CoFee system via callbackUrl"""
     logger.debug("Received %d clusters and %d segments from CoFee", len(clusters), len(segments))
-    store_text_clusters(exercise_id, clusters)
+    cluster_ids = store_text_clusters(exercise_id, clusters)
     store_text_blocks(segments, clusters)
-    connect_text_blocks_to_clusters(clusters)
+    connect_text_blocks_to_clusters(clusters, cluster_ids)
     logger.debug("Finished processing CoFee results")
