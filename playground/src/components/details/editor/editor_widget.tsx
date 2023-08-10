5f1a130dd7ec105ec2d54ae7157bb72e8f470be0
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2023-08-10 23:17:26 +0200

    change comment

--- playground/src/components/details/editor/editor_widget.tsx
+++ playground/src/components/details/editor/editor_widget.tsx
@@ -76,9 +76,9 @@ export function EditorWidget({ editor, children, afterLineNumber, afterColumn, m
   useEffect(() => {
     updateWidget();
 
-    // Last resort workaround for the widget not being created
+    // Last resort workaround for the view zone not being created
     const observer = new MutationObserver((mutationsList, observer) => {
-      // If the widget zone exists but the overlay widget doesn't exist, update it
+      // If the widget exists but the view zone doesn't, update everything
       if (!document.getElementById(`widget-${id}-zone`)) {
         updateWidget();
       }
