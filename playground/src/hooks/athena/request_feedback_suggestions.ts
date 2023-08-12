5615eccb7110fc1ff5d98e0bd7263fe9adf9c68f
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2023-08-12 23:51:22 +0200

    add fix before I forget

--- playground/src/hooks/athena/request_feedback_suggestions.ts
+++ playground/src/hooks/athena/request_feedback_suggestions.ts
@@ -27,6 +27,7 @@ export default function useRequestFeedbackSuggestions(
       let response = await athenaFetcher("/feedback_suggestions", { exercise, submission });
       if (response?.data) {
         response.data.feedbacks = response.data.map((feedback: Feedback) => {
+          feedback.id = Date.now(); // Good enough for the playground
           feedback.type = exercise.type;
           feedback.isSuggestion = true;
           return feedback;
