faebd85588356a2e7f45b6e381c6cbece3db7927
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2023-08-12 16:42:09 +0200

    ???

--- module_text_llm/module_text_llm/helpers/models/openai.py
+++ module_text_llm/module_text_llm/helpers/models/openai.py
@@ -80,7 +80,7 @@ def _use_azure_credentials():
     openai.api_key = os.environ.get("LLM_AZURE_OPENAI_API_KEY")
     openai.api_base = os.environ.get("LLM_AZURE_OPENAI_API_BASE")
     # os.environ.get("LLM_AZURE_OPENAI_API_VERSION")
-    openai.api_version = "2022-06-01-preview"
+    openai.api_version = "2023-03-15-preview"
 
 
 def _use_openai_credentials():
