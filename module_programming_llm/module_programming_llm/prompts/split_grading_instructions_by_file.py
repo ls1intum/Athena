3ddb848028aeff091afd22217edb8eda1d60a45a
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2023-08-19 21:30:15 +0200

    change prompt

--- module_programming_llm/module_programming_llm/prompts/split_grading_instructions_by_file.py
+++ module_programming_llm/module_programming_llm/prompts/split_grading_instructions_by_file.py
@@ -1,15 +1,18 @@
 system_template = """\
 You are an AI tutor at a prestigious university tasked with grading and providing feedback to programming assignments.
 
-Restructure the grading instructions by changed file.
+Restructure the grading instructions by changed file to make it simpler.
 """
 
 human_template = """\
 Grading instructions:
 {grading_instructions}
 
-Changed files:
-{changed_files}
+Changed files from template to sample solution:
+{changed_files_from_template_to_solution}
+
+Changed files from template to student submission:
+{changed_files_from_template_to_submission}
 
 Grading instructions by file:
 """
--- module_programming_llm/module_programming_llm/prompts/split_problem_statement_by_file.py
+++ module_programming_llm/module_programming_llm/prompts/split_problem_statement_by_file.py
@@ -8,8 +8,11 @@ human_template = """\
 Problem statement:
 {problem_statement}
 
-Changed files:
-{changed_files}
+Changed files from template to sample solution:
+{changed_files_from_template_to_solution}
+
+Changed files from template to student submission:
+{changed_files_from_template_to_submission}
 
 Problem statement by file:
 """
