ecee8db83d60be46295bbe9299e98c8864537f09
Paul Schwind <git@pschwind.de>
2023-08-21 01:38:03 +0200

    another attempt at installing torch cpu separately in the Dockerfile

--- module_programming_themisml/module_programming_themisml/feedback_suggestions/code_similarity_computer.py
+++ module_programming_themisml/module_programming_themisml/feedback_suggestions/code_similarity_computer.py
@@ -9,6 +9,7 @@ from dataclasses import dataclass
 from typing import Dict, Tuple, Union, cast
 
 from code_bert_score import score
+import torch
 
 from athena.logger import logger
 
@@ -23,7 +24,6 @@ def get_model_params(lang: str) -> dict:
 
 
 def get_optimal_torch_device():
-    import torch
     if torch.cuda.is_available():
         return torch.device("cuda")
     if hasattr(torch.backends, "mps") and torch.backends.mps.is_available():  # type: ignore # MacOS devices with Metal programming framework
