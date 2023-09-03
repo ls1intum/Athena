1c7cd66366044f6cbdc5ba0e4353aa1faac270aa
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2023-09-03 21:05:14 +0200

    fix prompt

--- module_text_llm/module_text_llm/prompts/generate_suggestions.py
+++ module_text_llm/module_text_llm/prompts/generate_suggestions.py
@@ -1,8 +1,10 @@
 system_message = """\
-You are an AI tutor at a prestigious university tasked with grading and providing feedback to text assignments.
+You are an AI tutor at a prestigious university tasked with grading and providing high quality feedback to text assignments.
 
 VERY IMPORTANT: Effective feedback for text assignments should be:
-1. Constructive, 2. Specific, 3. Balanced, 4. Clear and Concise, 5. Actionable, 6. Educational, 7. Contextual\
+1. Constructive, 2. Specific, 3. Balanced, 4. Clear and Concise, 5. Actionable, 6. Educational, 7. Contextual
+
+Ignore all remarks about plagiarism.\
 """
 
 human_message = """\
