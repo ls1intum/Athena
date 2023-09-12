4f0bca963245ccbc69051d93560e2d30f435bd89
Paul Schwind <git@pschwind.de>
2023-09-13 01:47:39 +0200

    constrain memory usage

---
+++ module_programming_themisml/module_programming_themisml/feedback_suggestions/batch.py
@@ -0,0 +1,12 @@
+from itertools import islice, zip_longest
+
+# https://docs.python.org/3/library/itertools.html#itertools-recipes
+def batched(iterable, n):
+    "Batch data into lists of length n. The last batch may be shorter."
+    # batched('ABCDEFG', 3) --> ABC DEF G
+    it = iter(iterable)
+    while True:
+        batch = list(islice(it, n))
+        if not batch:
+            return
+        yield batch
--- module_programming_themisml/module_programming_themisml/feedback_suggestions/feedback_suggestions.py
+++ module_programming_themisml/module_programming_themisml/feedback_suggestions/feedback_suggestions.py
@@ -1,10 +1,11 @@
-from typing import Dict, List, Tuple
-from athena.helpers.programming.feedback import format_feedback_title
+from typing import Dict, Iterable, List
 
+from athena.helpers.programming.feedback import format_feedback_title
 from athena.logger import logger
 from athena.programming import Feedback, Submission
 
 from module_programming_themisml.extract_methods import MethodNode, extract_methods
+from module_programming_themisml.feedback_suggestions.batch import batched
 from .code_similarity_computer import CodeSimilarityComputer
 
 SIMILARITY_SCORE_THRESHOLD = 0.95  # has to be really high - otherwise, there would just be too many feedback suggestions
@@ -27,28 +28,37 @@ def make_feedback_suggestion_from(feedback: Feedback, submission: Submission, su
     return suggestion
 
 
-def create_feedback_suggestions(
-    submissions: List[Submission],
-    feedbacks: List[Feedback],
-) -> List[Feedback]:
-    """
-    Get a list of all submissions that the given feedback could also apply to (similar code in same method).
-    Then generate feedback suggestions for those submissions.
-    """
-    if len(feedbacks) == 0:
-        return []  # nothing to do
+class CodeComparisonWithCorrespondingSuggestions:
+    """A pair of code snippets with a corresponding suggestions if their similarity is high enough."""
+    def __init__(self, code1: str, code2: str, suggestion: Feedback) -> None:
+        self.code1 = code1
+        self.code2 = code2
+        self.suggestion = suggestion
+    
+    def has_same_code(self, other) -> bool:
+        return self.code1 == other.code1 and self.code2 == other.code2
 
-    # group feedbacks by file path for faster access
-    logger.debug("Grouping %d feedbacks by file path", len(feedbacks))
+
+def group_feedbacks_by_file_path(feedbacks: List[Feedback]) -> Dict[str, List[Feedback]]:
+    """Groups feedbacks by file path for faster access."""
     feedbacks_by_file_path: Dict[str, List[Feedback]] = {}
     for feedback in feedbacks:
         if feedback.file_path not in feedbacks_by_file_path:
             feedbacks_by_file_path[str(feedback.file_path)] = []
         feedbacks_by_file_path[str(feedback.file_path)].append(feedback)
+    return feedbacks_by_file_path
 
-    # create suggestions that will be given if the similarity is high
-    logger.debug("Reading code from %d submissions", len(submissions))
-    code_comparisons_with_suggestions: Dict[Tuple[str, str], List[Feedback]] = {}  # key: (code1, code2), value: list of suggestions to give if the similarity is high
+
+def create_comparisons_with_suggestions(
+    submissions: List[Submission],
+    feedbacks: List[Feedback],
+) -> Iterable[CodeComparisonWithCorrespondingSuggestions]:
+    """Creates code comparisons and corresponding feedback suggestions as a generator."""
+    if len(feedbacks) == 0:
+        return
+    # group feedbacks by file path for faster access
+    logger.debug("Grouping %d feedbacks by file path", len(feedbacks))
+    feedbacks_by_file_path = group_feedbacks_by_file_path(feedbacks)
     for submission in submissions:
         for file_path, file_feedbacks in feedbacks_by_file_path.items():
             # read file from submission
@@ -71,32 +81,43 @@ def create_feedback_suggestions(
                     if feedback.meta["method_name"] == s_method.name:
                         # compare code (later) and add feedback as a possible suggestion (also later)
                         suggestion = make_feedback_suggestion_from(feedback, submission, s_method)
-                        comparison_key = (s_method.source_code, feedback.meta["method_code"])
-                        if comparison_key not in code_comparisons_with_suggestions:
-                            code_comparisons_with_suggestions[comparison_key] = []
-                        code_comparisons_with_suggestions[comparison_key].append(suggestion)
+                        yield CodeComparisonWithCorrespondingSuggestions(s_method.source_code, feedback.meta["method_code"], suggestion)
 
-    # compute similarity scores for all comparisons at once
-    sim_computer = CodeSimilarityComputer()
-    for code1, code2 in code_comparisons_with_suggestions:
-        sim_computer.add_comparison(code1, code2)
-    logger.debug("Computing similarity scores for %d code comparisons", len(code_comparisons_with_suggestions))
-    sim_computer.compute_similarity_scores()  # compute all at once, enables vectorization
 
-    # create suggestions
+def create_feedback_suggestions(
+    submissions: List[Submission],
+    feedbacks: List[Feedback],
+) -> List[Feedback]:
+    """
+    Get a list of all submissions that the given feedback could also apply to (similar code in same method).
+    Then generate feedback suggestions for those submissions.
+    """
+    if len(feedbacks) == 0:
+        return []  # nothing to do
+
     suggestions: List[Feedback] = []
-    for (code1, code2), suggestions_for_comparison in code_comparisons_with_suggestions.items():
-        similarity = sim_computer.get_similarity_score(code1, code2)
-        if similarity.f1 >= SIMILARITY_SCORE_THRESHOLD:
-            # found similar code -> create feedback suggestion
-            logger.info("Found similar code with similarity score %s: %s", similarity.f1, suggestions_for_comparison)
-            for suggestion_to_give in suggestions_for_comparison:
+
+    # create code comparisons and corresponding feedback suggestions in batches for less memory usage
+    for idx, comparisons_with_suggestions in enumerate(batched(create_comparisons_with_suggestions(submissions, feedbacks), 128)):
+        # compute similarity scores for all comparisons at once
+        sim_computer = CodeSimilarityComputer()
+        for s_comp in comparisons_with_suggestions:
+            sim_computer.add_comparison(s_comp.code1, s_comp.code2)
+        logger.debug("Computing similarity scores for %d code comparisons (batch #%d)", len(comparisons_with_suggestions), idx)
+        sim_computer.compute_similarity_scores()  # compute all at once, enables vectorization
+
+        # create suggestions
+        for s_comp in comparisons_with_suggestions:
+            similarity = sim_computer.get_similarity_score(s_comp.code1, s_comp.code2)
+            if similarity.f1 >= SIMILARITY_SCORE_THRESHOLD:
+                # found similar code -> create feedback suggestion
+                logger.info("Found similar code with similarity score %s", similarity.f1)
                 # add meta information for debugging
-                suggestion_to_give.meta["precision_score"] = similarity.precision
-                suggestion_to_give.meta["recall_score"] = similarity.recall
-                suggestion_to_give.meta["similarity_score"] = similarity.f1
-                suggestion_to_give.meta["similarity_score_f3"] = similarity.f3
+                s_comp.suggestion.meta["precision_score"] = similarity.precision
+                s_comp.suggestion.meta["recall_score"] = similarity.recall
+                s_comp.suggestion.meta["similarity_score"] = similarity.f1
+                s_comp.suggestion.meta["similarity_score_f3"] = similarity.f3
                 # add to suggestions
-                suggestions.append(suggestion_to_give)
+                suggestions.append(s_comp.suggestion)
 
     return suggestions
