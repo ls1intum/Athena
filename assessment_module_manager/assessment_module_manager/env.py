cf8bd83af7115dbb5889c35ec9969522674a88ca
Dmytro Polityka <dmytro.polityka@tum.de>
2024-05-19 23:43:15 +0200

    remove dotenv and unused code

--- assessment_module_manager/assessment_module_manager/env.py
+++ assessment_module_manager/assessment_module_manager/env.py
@@ -5,12 +5,6 @@ from assessment_module_manager.deployment import list_deployments
 from assessment_module_manager.module.list_modules import list_modules
 
 PRODUCTION = os.environ.get("PRODUCTION", "0") == "1"
-SECRET = os.getenv("SECRET")  # Artemis <-> Athena
-if SECRET is None:
-    if PRODUCTION == "1":
-        raise ValueError("Missing SECRET environment variable. "
-                         "Set it to a random string to secure the communication between the LMS and the assessment module manager.")
-    SECRET = "abcdef12345"  # noqa: This secret is only used for development setups for simplicity
 
 MODULE_SECRETS = {}
 for module in list_modules():
@@ -25,5 +19,7 @@ for deployment in list_deployments():
     secret = os.environ.get(f"ARTEMIS_{deployment.name.upper()}_SECRET")
     if secret is None and PRODUCTION:
         raise ValueError(f"Missing secret for Artemis deployment {deployment.name}. "
-                         f"Set the {deployment.name.upper()}_SECRET environment variable.")
+                         f"Set the {deployment.name.upper()}_SECRET environment variable to secure the communication "
+                         f"between the LMS and the assessment module manager.")
+    secret = "abcdef12345"  # noqa: This secret is only used for development setups for simplicity
     DEPLOYMENT_SECRETS[deployment.url] = secret
