8ace5c64f55868ac5f3d94bebace98ae217f4f7f
= Enea_Gore <ge72git@mytum.de>
2024-07-10 10:54:09 +0200

    some cleanup

--- shared_llm/shared_llm/helpers/models/test_llama.py
+++ shared_llm/shared_llm/helpers/models/test_llama.py
@@ -9,11 +9,7 @@ from pydantic import validator, Field, PositiveInt
 from langchain.base_language import BaseLanguageModel
 import os
 from langchain_community.chat_models import ChatOllama # type: ignore
-# What is our goal with this class for today, we want to:
-# XXX 1 XXX -> Have a general Llama model config that can be used and retrieved from the playground
-# XXX 2 XXX -> Not sure if we need anything else tbh
 
-# TODO Figure out how and where to select the ollama in the playground
 dotenv.load_dotenv()
 
 
@@ -21,12 +17,7 @@ dotenv.load_dotenv()
 url = 'https://gpu-artemis.ase.cit.tum.de/ollama/api/generate'
 ollama_endpoint = 'https://gpu-artemis.ase.cit.tum.de/ollama'
 
-
-# Lets tell this thing to do some specific format for the response
-# should it be part of the system message ? i guess it makes some sense 
 auth = HTTPBasicAuth(os.environ["GPU_USER"],os.environ["GPU_PASSWORD"])
-
-
 
 headers2={
     'Authorization': requests.auth._basic_auth_str(os.environ["GPU_USER"],os.environ["GPU_PASSWORD"]) # type: ignore
@@ -43,12 +34,6 @@ llm = ChatOllama(
             headers ={
     'Authorization': requests.auth._basic_auth_str(os.environ["GPU_USER"],os.environ["GPU_PASSWORD"]) # type: ignore
     },
-            
-            # temperature = 1,
-            # mirostat =2,
-            # top_p = 1,
-            # cache = False, # for whatever reason, num predict was not allowed here. probably would have to change model
-            # kind of a problem considering that llama3 sometimes generates without end
             )
  
 # Set the authentication
@@ -105,7 +90,6 @@ def call_ollama(system_message, prompt):
         
 
 def chain_of_thought():
-    # How could a chain of thought prompting look like, well lets have a look
     return 0
 
 class OllamaInstance():
@@ -172,54 +156,5 @@ if __name__ == "__main__":
             
                 )
 
-        # prompt = ChatPromptTemplate.from_messages([
-        #     ("system", "You are a helpful AI instructor for a course of Software Engineering."),
-        #     ("human", "The problem statement is: Name 3 Patterns of software engineering."),
-        #     ("ai", "Provide your answer and i will give you helpful feedback to improve."),
-        #     ("human", "{user_input}"),
-        # ])
-        
-        #     # llm.invoke("hello ollama")
-    
     ollama = OllamaInstance()
-    # print(ollama.invoke_this_thing("test again whats wrong with langchain omg"))
     print(ollama.llm.invoke("why would this not work"))
-    # while(True):
-    #     query = input("LLM input: ")
-    #     paragraph = []
-    #     buffer = ""
-
-    #     # for chunk in llm.stream(query):
-    #     #     paragraph.append(chunk.strip())  # Strip leading and trailing whitespace from each chunk
-
-    #     # # Join all the chunks into a single string with spaces
-    #     result = llm.invoke(query)
-    #     print(result)
-        # for chunk in llm.stream(query):
-        #     buffer += chunk  # Append each chunk to the buffer
-        #     words = buffer.split(' ')  # Split buffer into words
-        #     paragraph.extend(words[:-1])  # Add all complete words to the paragraph list
-        #     buffer = words[-1]  # Keep the last word in the buffer (it might be incomplete)
-        # if buffer:
-        #     paragraph.append(buffer)
-        #     print(buffer)
-        # formatted_text = ' '.join(paragraph)
-
-        # # for chunks in llm.stream(query):
-        # #     print(chunks)
-        # print(formatted_text)
-
-       
-
-        # system_message = """ 
-        # You are an AI tutor for text assessment at a prestigious university. Be super professional and give the student feedback. ONLY adhere to the json schema for the response provided below.
-        # The student will respond in the prompt.
-        # # Problem statement
-        # {problem_statement}
-
-        # Disect the student submission and highlight on what the student is getting feedback on.
-        # Response in a format as follows:
-        # {["student_sentence" : "your_feedback"]}
-        # """
-        # prompt = "The sky is kind of blue sometimes but sometimes not."
-        # call_ollama(system_message,prompt)
