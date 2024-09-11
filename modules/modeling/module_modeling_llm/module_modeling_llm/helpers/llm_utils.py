2cb193e8cf9e668abc2d833c0d2949a85dceccd8
leon <leon.wehrhahn@tum.de>
2024-09-11 19:49:15 +0700

    Update launch.json and llm_utils.py

--- modules/modeling/module_modeling_llm/module_modeling_llm/helpers/llm_utils.py
+++ modules/modeling/module_modeling_llm/module_modeling_llm/helpers/llm_utils.py
@@ -4,6 +4,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.pydantic_v1 import BaseModel, ValidationError
 from langchain_core.runnables import RunnableSequence
 from athena import get_experiment_environment
+from assessment_module_manager.logger import logger
 
 T = TypeVar("T", bound=BaseModel)
 
@@ -45,9 +46,7 @@ async def predict_and_parse(
     try:
         return await chain.ainvoke(prompt_input, config={"tags": tags})
     except (ValidationError) as e:
-        # In the future, we should probably have some recovery mechanism here (i.e. fix the output with another prompt)
-        # Show the exception
-
+        
         logger.error("Exception type: %s, Message: %s", type(e).__name__, e)
 
         return None
