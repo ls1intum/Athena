acf5d269e652f15b061e8a68d3f918c2d5345728
Leon Laurin Wehrhahn <58460654+LeonWehrhahn@users.noreply.github.com>
2024-09-13 01:34:27 +0700

    Preliminary AI Feedback for all Apollon UML Diagrams (#335)
    Co-authored-by: Dmytro Polityka <33299157+undernagruzez@users.noreply.github.com>

--- modules/modeling/module_modeling_llm/module_modeling_llm/helpers/llm_utils.py
+++ modules/modeling/module_modeling_llm/module_modeling_llm/helpers/llm_utils.py
@@ -1,120 +1,12 @@
 from typing import Optional, Type, TypeVar, List
-
-import tiktoken
-from langchain.base_language import BaseLanguageModel
-from langchain.chains import LLMChain
-from langchain.chains.openai_functions import create_structured_output_chain
-from langchain.chat_models import ChatOpenAI
-from langchain.output_parsers import PydanticOutputParser
-from langchain.prompts import (
-    ChatPromptTemplate,
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-)
-from langchain.schema import OutputParserException
-from pydantic import BaseModel, ValidationError
-
-from athena import emit_meta, get_experiment_environment
+from langchain_core.language_models import BaseLanguageModel
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.pydantic_v1 import BaseModel, ValidationError
+from langchain_core.runnables import RunnableSequence
+from athena import get_experiment_environment
+from athena.logger import logger
 
 T = TypeVar("T", bound=BaseModel)
-
-
-def num_tokens_from_string(string: str) -> int:
-    """Returns the number of tokens in a text string."""
-    encoding = tiktoken.get_encoding("cl100k_base")
-    num_tokens = len(encoding.encode(string))
-    return num_tokens
-
-
-def num_tokens_from_prompt(chat_prompt: ChatPromptTemplate, prompt_input: dict) -> int:
-    """Returns the number of tokens in a chat prompt."""
-    return num_tokens_from_string(chat_prompt.format(**prompt_input))
-
-
-def check_prompt_length_and_omit_features_if_necessary(prompt: ChatPromptTemplate,
-                                                       prompt_input: dict,
-                                                       max_input_tokens: int,
-                                                       omittable_features: List[str],
-                                                       debug: bool):
-    """Check if the input is too long and omit features if necessary.
-
-    Note: Omitted features will be replaced with "omitted" in the prompt
-
-    Args:
-        prompt (ChatPromptTemplate): Prompt template
-        prompt_input (dict): Prompt input
-        max_input_tokens (int): Maximum number of tokens allowed
-        omittable_features (List[str]): List of features that can be omitted, ordered by priority (least important first)
-        debug (bool): Debug flag
-
-    Returns:
-        (dict, bool): Tuple of (prompt_input, should_run) where prompt_input is the input with omitted features and 
-                      should_run is True if the model should run, False otherwise
-    """
-    if num_tokens_from_prompt(prompt, prompt_input) <= max_input_tokens:
-        # Full prompt fits into LLM context => should run with full prompt
-        return prompt_input, True
-
-    omitted_features = []
-
-    # Omit features until the input is short enough
-    for feature in omittable_features:
-        if feature in prompt_input:
-            omitted_features.append(feature)
-            prompt_input[feature] = "omitted"
-            if num_tokens_from_prompt(prompt, prompt_input) <= max_input_tokens:
-                if debug:
-                    emit_meta("omitted_features", omitted_features)
-                return prompt_input, True
-
-    # If we get here, we couldn't omit enough features
-    return prompt_input, False
-
-
-def supports_function_calling(model: BaseLanguageModel):
-    """Returns True if the model supports function calling, False otherwise
-
-    Args:
-        model (BaseLanguageModel): The model to check
-
-    Returns:
-        boolean: True if the model supports function calling, False otherwise
-    """
-    return isinstance(model, ChatOpenAI)
-
-
-def get_chat_prompt_with_formatting_instructions(
-        model: BaseLanguageModel,
-        system_message: str,
-        human_message: str,
-        pydantic_object: Type[T]
-) -> ChatPromptTemplate:
-    """Returns a ChatPromptTemplate with formatting instructions (if necessary)
-
-    Note: Does nothing if the model supports function calling
-
-    Args:
-        model (BaseLanguageModel): The model to check if it supports function calling
-        system_message (str): System message
-        human_message (str): Human message
-        pydantic_object (Type[T]): Model to parse the output
-
-    Returns:
-        ChatPromptTemplate: ChatPromptTemplate with formatting instructions (if necessary)
-    """
-    if supports_function_calling(model):
-        system_message_prompt = SystemMessagePromptTemplate.from_template(system_message)
-        human_message_prompt = HumanMessagePromptTemplate.from_template(human_message)
-        return ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
-
-    output_parser = PydanticOutputParser(pydantic_object=pydantic_object)
-    system_message_prompt = SystemMessagePromptTemplate.from_template(system_message + "\n{format_instructions}")
-    system_message_prompt.prompt.partial_variables = {"format_instructions": output_parser.get_format_instructions()}
-    system_message_prompt.prompt.input_variables.remove("format_instructions")
-    human_message_prompt = HumanMessagePromptTemplate.from_template(
-        human_message + "\n\nJSON response following the provided schema:")
-    return ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
-
 
 async def predict_and_parse(
         model: BaseLanguageModel,
@@ -145,19 +37,16 @@ async def predict_and_parse(
     if experiment.run_id is not None:
         tags.append(f"run-{experiment.run_id}")
 
-    if supports_function_calling(model):
-        chain = create_structured_output_chain(pydantic_object, llm=model, prompt=chat_prompt, tags=tags)
+    structured_output_llm = model.with_structured_output(pydantic_object, method="json_mode")
+    chain = RunnableSequence(
+        chat_prompt,
+        structured_output_llm
+    )
 
-        try:
-            return await chain.arun(**prompt_input)
-        except (OutputParserException, ValidationError):
-            # In the future, we should probably have some recovery mechanism here (i.e. fix the output with another prompt)
-            return None
-
-    output_parser = PydanticOutputParser(pydantic_object=pydantic_object)
-    chain = LLMChain(llm=model, prompt=chat_prompt, output_parser=output_parser, tags=tags)
     try:
-        return await chain.arun(**prompt_input)
-    except (OutputParserException, ValidationError):
-        # In the future, we should probably have some recovery mechanism here (i.e. fix the output with another prompt)
+        return await chain.ainvoke(prompt_input, config={"tags": tags})
+    except (ValidationError) as e:
+
+        logger.error("Exception type: %s, Message: %s", type(e).__name__, e)
+
         return None
--- modules/modeling/module_modeling_llm/module_modeling_llm/prompts/submission_format/bpmn.py
+++
@@ -1,2 +0,0 @@
-submission_format_remarks = """The submissions are provided as BPMN 2.0 XML and need to match the following problem 
-statement."""
