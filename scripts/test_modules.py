9ca5c89893fa05d2b2fea61d688949938a0076aa
Dmytro Polityka <dmytro.polityka@tum.de>
2025-03-06 19:46:08 +0100

    change invocation

--- scripts/test_modules.py
+++ scripts/test_modules.py
@@ -14,9 +14,15 @@ def main():
     test_modules = ["tests/integration_tests"]
 
     success = True
+    original_env = os.environ["PATH"]
 
     for module in test_modules:
-        result = subprocess.run(["poetry", "run", "pytest"], cwd=module)
+        path_to_module_environment = os.path.join(os.getcwd(), module, ".venv")
+        os.environ["VIRTUAL_ENV"] = path_to_module_environment
+        os.environ["PATH"] = os.path.join(path_to_module_environment, "bin") + os.pathsep + original_env
+        subprocess.run([sys.executable, "-m", "venv", path_to_module_environment])
+
+        result = subprocess.run(["poetry", "run", "pytest"], cwd=path_to_module_environment)
         if result.returncode != 0:
             success = False
 
